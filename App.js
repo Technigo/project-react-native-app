65142d24ebacfa9f95d32ab204bd42b6d66a28a1
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-12-16 17:00:55 +0100

    Not a step forward at all

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React from "react"
 import styled from "styled-components/native"
 
+// const apiKey = `563492ad6f917000010000016674d16c530e444482c459f1837b2a47`
+// // const [ pics, setPics ] = useState ([])
+// const url = "https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1"
+
+
+// fetch(url, { headers: { Authorization: 'xyz' }} 
+// useEffect(() => {
+//     fetch("url", { headers: { Authorization: 'apiKey'}})
+//         .then(res => res.json())
+//         .then(json => {
+//           setPics(json)
+//           console.log(json)
+//         })
+// }, [])
+
 const App = () => {
   return (
     <Container>
@@ -10,6 +25,21 @@ const App = () => {
     </Container>
   )
 }
+
+// const [ pics, setPics ] = useState ([])
+
+// useEffect(() => {
+//   fetch(`https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1
+//   `)
+//   .then((res) => res.json())
+//   .then ((json) => {
+//     setPics(json)
+//     console.log(json)
+//   })
+// }, [])
+
+
+
 
 const Container = styled.View`
   flex: 1;
