2f99d8a330559707a54e00918c0c229dfc488517
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-12-17 12:01:35 +0100

    fetch works in console

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native"
 
-//export const pics = () {
-//const [pics, setPics] = useState([])
-
-import React, { useState } from "react"
-import styled from "styled-components/native"
-
 const apiKey = `563492ad6f917000010000016674d16c530e444482c459f1837b2a47`
 const url = "https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1"
 
@@ -14,14 +8,15 @@ const App = () => {
   const [pics, setPics] = useState([])
 
   useEffect(() => {
-      fetch(url, { headers: { Authorization: apiKey}})
-        .then(json => {
-          setPics(json)
-          console.log(json)
-        })
-      }, []);
+    fetch(url, { headers: { Authorization: apiKey } })
+      .then(res => res.json())
+      .then(json => {
+        setPics(json)
+        console.log(json)
+      })
+  }, []);
       
-const App = () => {
+/*const App = () => {*/
   return (
     <Container>
       <Title>This is your cool app!</Title>
