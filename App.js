541b5afa8d8a73af2f11fff5c5806be03d4c3614
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-12-17 13:46:15 +0100

    map is not a function

--- App.js
+++ App.js
@@ -2,28 +2,37 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components/native"
 
 const apiKey = `563492ad6f917000010000016674d16c530e444482c459f1837b2a47`
-const url = "https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1"
+const url = "https://api.pexels.com/v1/search?query=example+crossfit&per_page=15&page=1"
+/*const [photos, setPhotos] = useState([])*/
 
 const App = () => {
-  const [pics, setPics] = useState([])
+  const [photos, setPhotos] = useState([])
 
   useEffect(() => {
     fetch(url, { headers: { Authorization: apiKey } })
       .then(res => res.json())
       .then(json => {
-        setPics(json)
+        setPhotos(json)
         console.log(json)
       })
-  }, []);
+  }, [])
       
-/*const App = () => {*/
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
+      {photos.map((photos) => {
+        return (
+        key={photos}
+          <h3>{'photos.photographer'}</h3>
+      )) } 
+        
+        <Title>This is your cool app!</Title>
+        <Title>Go to App.js and start coding</Title>
+        <Title>💅💅💅</Title>
+      
+      </Container>
   )
+   
 }
 
 const Container = styled.View`
