b63a4919d87aade70ca93849ed9281cf074e43f2
Petra B <petrabrinck@icloud.com>
2019-12-17 14:50:43 +0100

    tried to publish the photo array but inspite help from Damien we could get it to work. Backed and now we have the photo array back

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native"
+// import { View, Text } from 'react native'
+
 const apiKey = `563492ad6f917000010000016674d16c530e444482c459f1837b2a47`
-const url = "https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1"
+const url = "https://api.pexels.com/v1/search?query=example+health&per_page=15&page=1"
+
 const App = () => {
-  const [pics, setPics] = useState([])
+  const [photos, setPhotos] = useState([])
   useEffect(() => {
     fetch(url, { headers: { Authorization: apiKey } })
       .then(res => res.json())
       .then(json => {
-        setPics(json)
+        setPhotos(json.photos)
         console.log(json)
       })
   }, []);
+
   /*const App = () => {*/
   return (
     <Container>
-      <Title>This is your cool app!</Title>
+      {/* return ( */}
+      {/* {photos.map((photo) => (
+        `<View key={photo.id}>{photo.photographer}</View>`
+      ))} */}
+
+      <Title>Petra, this is your cool app!</Title>
       <Title>Go to App.js and start coding</Title>
       <Title>💅💅💅</Title>
-    </Container>
+    </Container >
   )
 }
+
 const Container = styled.View`
   flex: 1;
   background-color: papayawhip;
