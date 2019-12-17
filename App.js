bb34691905103f5e8c2d29b3fce7f8b03f06db2c
Petra B <petrabrinck@icloud.com>
2019-12-17 12:05:50 +0100

    first commit and we can see the array in the consol log

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native"
-
+const apiKey = `563492ad6f917000010000016674d16c530e444482c459f1837b2a47`
+const url = "https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1"
 const App = () => {
+  const [pics, setPics] = useState([])
+  useEffect(() => {
+    fetch(url, { headers: { Authorization: apiKey } })
+      .then(res => res.json())
+      .then(json => {
+        setPics(json)
+        console.log(json)
+      })
+  }, []);
+  /*const App = () => {*/
   return (
     <Container>
       <Title>This is your cool app!</Title>
@@ -10,17 +21,14 @@ const App = () => {
     </Container>
   )
 }
-
 const Container = styled.View`
   flex: 1;
   background-color: papayawhip;
   justify-content: center;
   align-items: center;
 `
-
 const Title = styled.Text`
   font-size: 24px;
   color: palevioletred;
 `
-
 export default App
