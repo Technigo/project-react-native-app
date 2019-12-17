e93930903d1efd64f47fc7dc8f51e2b95bc24abb
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-12-17 11:34:56 +0100

    trying to make it fetch

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native"
 
+//export const pics = () {
+//const [pics, setPics] = useState([])
+
+import React, { useState } from "react"
+import styled from "styled-components/native"
+
+const apiKey = `563492ad6f917000010000016674d16c530e444482c459f1837b2a47`
+const url = "https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1"
+
+const App = () => {
+  const [pics, setPics] = useState([])
+
+  useEffect(() => {
+      fetch(url, { headers: { Authorization: apiKey}})
+        .then(json => {
+          setPics(json)
+          console.log(json)
+        })
+      }, []);
+      
 const App = () => {
   return (
     <Container>
