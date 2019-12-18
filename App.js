ea3a148b4907bc944143ebc14e58e6c48e91d994
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-12-18 14:32:29 +0100

    just trying out and need a break

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component } from "react";
 import styled from "styled-components/native"
-import {View, Text} from 'react-native';
+import { Image, View } from 'react-native';
 
 const apiKey = `563492ad6f917000010000016674d16c530e444482c459f1837b2a47`
 const url = "https://api.pexels.com/v1/search?query=example+crossfit&per_page=15&page=1"
-/*const [photos, setPhotos] = useState([])*/
+
 
 const App = () => {
   const [photos, setPhotos] = useState([])
@@ -17,21 +17,21 @@ const App = () => {
         console.log(json)
       })
   }, [])
-      
+     
 
-  return (
+  return(
     <Container>
-      return (
-      {photos.map((photo) => (
-  <View key={photo.id}>$`{photo.photographer}`></View>
-))}
-        <Text>This is your cool app!</Text>
-        <Text>Go to App.js and start coding</Text>
-        <Text>💅💅💅</Text  >
-      )
-      </Container>
-  )
-   
+     
+          return (
+            <View>
+              <Image 
+                source={{uri: 'https://api.pexels.com/v1/search?query=example+crossfit&per_page=15&page=1'}}
+                style={{width: 400, height: 400}} />
+            </View>
+          )}
+        }
+    </Container>
+  )   
 }
 
 const Container = styled.View`
@@ -41,10 +41,4 @@ const Container = styled.View`
   align-items: center;
 `
 
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
-
 export default App
-
