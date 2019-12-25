b107e9bfdbb6b897b3bfc67866ba81188dff768c
Angelica Brodin <brodin.angelica@gmail.com>
2019-12-25 13:43:47 +0100

    created a movie app, with a separate component

---
+++ components/Movie.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const Container = styled.SafeAreaView`
+    flex:1;
+    width:100%;
+    height:100%;
+    align-items:center;
+`
+// const Text = styled.Text`
+//     color:white;
+//     font-size:25;
+// `
+const Image = styled.ImageBackground`
+   height:100%;
+   width:90%;
+`
+
+export const Movie = ({ movie }) => (
+    <Container>
+        <Image resizeMode="contain" source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }} />
+        {/* <Text>{movie.title}</Text> */}
+    </Container>
+)
