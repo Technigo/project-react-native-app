876949a8d6e8bef32996d25af64f60c6060add87
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2019-12-28 02:11:30 +0100

    buttons styled on MoreInfo

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import styled from "styled-components/native";
-import { Text, Button, View, Image } from "react-native";
-
-import HomeScreen from "./components/HomeScreen";
-import MoreInfo from "./components/MoreInfo";
 
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+
+import HomeScreen from "./components/HomeScreen";
+import MoreInfo from "./components/MoreInfo";
 
 export const App = () => {
   return <AppContainer />;
@@ -22,6 +20,7 @@ const AppNavigator = createStackNavigator({
   MoreInfo: {
     screen: MoreInfo,
     navigationOptions: {
+      title: "Details",
       headerTintColor: "#00bfff",
       headerStyle: {
         backgroundColor: "#333333"
@@ -31,23 +30,5 @@ const AppNavigator = createStackNavigator({
 });
 
 const AppContainer = createAppContainer(AppNavigator);
-
-const Container = styled.View`
-  flex-direction: ${props => (props.row ? "row" : "column")};
-  background-color: #000;
-  height: ${props => (props.full ? "100%" : "auto")};
-  align-self: center;
-  width: 100%;
-`;
-
-const Title = styled.Text`
-  font-size: 14px;
-  color: palevioletred;
-
-  width: 100%;
-  margin: 0 auto;
-
-  height: 20;
-`;
 
 export default App;
--- components/MoreInfo.js
+++ components/MoreInfo.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import styled from "styled-components/native";
 
 import { Text, Button, View, Image } from "react-native";
+// import { Press } from "./Press";
 
 export const MoreInfo = ({ navigation }) => {
   const title = navigation.getParam("title");
@@ -10,22 +12,92 @@ export const MoreInfo = ({ navigation }) => {
   const description = navigation.getParam("description");
 
   return (
-    <View>
+    <Container>
       <Image
         source={image}
         style={{
-          width: 70,
-          height: 100,
-          marginVertical: 30,
-          marginLeft: 5
+          width: 100,
+          height: 150,
+          marginVertical: 30
         }}
       ></Image>
-      <Text>{title}</Text>
-      <Text>{authors}</Text>
-      <Text>{categories}</Text>
-      <Text>{description}</Text>
-    </View>
+      <Span>Title:</Span>
+      <Title title>{title}</Title>
+
+      <Span>Authors:</Span>
+      <Title title>{authors}</Title>
+
+      <Span>Category:</Span>
+      <Title>{categories}</Title>
+
+      <Span>Description:</Span>
+      <Title>{description}</Title>
+      <ContainerNoScroll>
+        <CTAButton>
+          <ButtonText>LEAVE REVIEW</ButtonText>
+        </CTAButton>
+        <CTAButton>
+          <ButtonText>SHARE</ButtonText>
+        </CTAButton>
+      </ContainerNoScroll>
+      <ContainerNoScroll marginBottom>
+        <Order>
+          <ButtonText white title marginBottom>
+            ORDER
+          </ButtonText>
+        </Order>
+      </ContainerNoScroll>
+    </Container>
   );
 };
+
+const Container = styled.ScrollView`
+  flex-direction: ${props => (props.row ? "row" : "column")};
+  background-color: #000;
+  color: #fff;
+  height: ${props => (props.full ? "100%" : "auto")};
+  align-self: ${props => (props.center ? "center" : "flex-start")};
+  padding-left: 10;
+  padding-right: 10;
+  width: ${props => (props.widthSmaller ? "80%" : "100%")};
+`;
+
+const ContainerNoScroll = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-bottom: ${props => (props.marginBottom ? 20 : 0)};
+`;
+
+const Title = styled.Text`
+  font-size: ${props => (props.title ? "15px" : "12px")};
+  color: #fff;
+  flex: 2;
+  margin-bottom: ${props => (props.marginBottom ? 0 : 2)};
+  line-height: 17;
+`;
+
+const Span = styled(Title)`
+  color: #00bfff;
+`;
+
+const ButtonText = styled.Text`
+  color: ${props => (props.white ? "#fff" : "#000")};
+  margin: auto auto;
+  font-size: ${props => (props.title ? "20px" : "18px")};
+`;
+
+const CTAButton = styled.TouchableOpacity`
+  margin-top: 20;
+  width: 40%;
+  height: 30;
+  background-color: #ffc000;
+`;
+
+const Order = styled(CTAButton)`
+  width: 90%;
+  height: 40;
+  background-color: #ff4000;
+`;
 
 export default MoreInfo;
