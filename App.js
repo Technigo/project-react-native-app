e7b0439da95ffb5eb7439996f78baec9f84be7ff
Jens_Nilsen <jensnilsen89@gmail.com>
2020-01-02 15:15:55 +0100

    preoject started but strugling with npm at the moment

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
-import React from "react"
-import styled from "styled-components/native"
+import React, { useState, useEffect } from "react";
+import styled from "styled-components/native";
 
 const App = () => {
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
+      <Title>Would You rather!?</Title>
+      <Title>heheheheheheheheh</Title>
       <Title>💅💅💅</Title>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.View`
   flex: 1;
   background-color: papayawhip;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Title = styled.Text`
   font-size: 24px;
   color: palevioletred;
-`
+`;
 
-export default App
+export default App;
