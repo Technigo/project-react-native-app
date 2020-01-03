aa916ec4e9e94f9622f7d9a00c173d08acd6af80
Jens_Nilsen <jensnilsen89@gmail.com>
2020-01-03 10:13:22 +0100

    buttton added. next up adding a homepage and adding shake funktion.

--- App.js
+++ App.js
@@ -2,42 +2,65 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components/native";
 import { ActivityIndicator, Button } from "react-native";
 
-const fetchWYR = async () => {
+const fetchChuck = async () => {
   const res = await fetch("https://api.chucknorris.io/jokes/random");
   const json = await res.json();
   return json;
 };
 
 const App = () => {
-  const [wouldYou, setWouldYou] = useState();
+  const [chuckState, setChuckState] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const fetchChuckData = () => {
+    setLoading(true);
+    fetchChuck().then(chuckData => {
+      setChuckState(chuckData);
+      setLoading(false);
+    });
+  };
 
   useEffect(() => {
-    fetchWYR().then(wouldYouData => {
-      setWouldYou(wouldYouData);
-    });
+    fetchChuckData();
   }, []);
 
   return (
     <Container>
       {loading ? (
-        <ActivityIndicator />
+        <ActivityIndicator size="large" color="#999" />
       ) : (
-        <Title>{wouldYou && wouldYou.value}</Title>
+        <Title>{chuckState && chuckState.value}</Title>
       )}
+
+      <Buttun onPress={fetchChuckData}>
+        <ButtonText>more chuck!</ButtonText>
+      </Buttun>
     </Container>
   );
 };
 
+const Buttun = styled.TouchableOpacity`
+  position: absolute;
+  background: white;
+  padding: 10px 20px;
+  bottom: 40;
+  border-radius: 10;
+`;
+
+const ButtonText = styled.Text`
+  font-size: 25px;
+`;
+
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
+  background-color: black;
   justify-content: center;
   align-items: center;
 `;
 
 const Title = styled.Text`
   font-size: 24px;
-  color: palevioletred;
+  color: white;
 `;
 
 export default App;
