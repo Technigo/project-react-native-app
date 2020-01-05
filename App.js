ee02239c6908e079b71c26695c9558f4e89f07c1
Nina Månsson <ninamansster@gmail.com>
2020-01-05 10:04:10 +0100

    api-call included

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styled from "styled-components/native"
+import { Image, View, Text, TouchableOpacity, Share, ScrollView, Dimensions, Alert, Vibration, Link } from 'react-native'
+
+const apiKey = `ceaaa691942f4f51925568bcf5183b80`
+const url = "https://newsapi.org/v2/everything?q=climate&from=2020-01-01"
 
 const App = () => {
+
+  const [news, setNews] = useState([])
+  useEffect(() => {
+    fetch(url,
+      {
+        headers:
+          { Authorization: apiKey }
+      })
+      .then(res => res.json())
+      .then(json => {
+        setNews(json.articles)
+
+      })
+  }, [])
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
+      <ScrollView>
+        <Title>This is your cool app, Nina!</Title>
+        <Title>Fantasticoç</Title>
+        <Title>💅💅💅</Title>
+        <Title>Climate News!</Title>
+        {news.map((articles) => (
+          <View key={articles.publishedAt}>
+            <Image
+              source={{ uri: articles.urlToImage }}
+              style={{ width: 310, height: 200 }} />
+            <Title>
+              {articles.title}
+            </Title>
+          </View>
+        ))}
+      </ScrollView>
     </Container>
   )
 }
+
+export default App
 
 const Container = styled.View`
   flex: 1;
@@ -23,4 +57,4 @@ const Title = styled.Text`
   color: palevioletred;
 `
 
-export default App
+
