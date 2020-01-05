154f85b86b4ae6bf12e68446d7b83fe085464744
nasimmhn <nasim.mahzoun@gmail.com>
2020-01-05 01:29:28 +0100

    added google map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export const MapScreen = () => {
   return (
-    <View>
-      <Text> LandingPageScreen </Text>
-    </View >
+
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        showsMyLocationButton={true}
+        showsUserLocation={true}
+        mapType={"standard"}
+        region={{
+          latitude: 59.3280118,
+          longitude: 18.0691595,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+      </MapView>
+    </View>
+
   )
 }
