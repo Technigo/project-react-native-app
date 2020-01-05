6831a3853ce677acc76b59a035097c65435b1053
Camilla <camillalofroth@hotmail.com>
2020-01-05 23:16:30 +0100

    reactnativeproject

--- App.js
+++ App.js
@@ -1,26 +1,69 @@
-import React from "react"
-import styled from "styled-components/native"
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components/native';
+import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
-const App = () => {
-  return (
-    <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
-  )
-}
 
-const Container = styled.View`
+//api and key
+
+const apiKey = `95622557-9a41-47af-a755-23f39b9b9675`
+const url = `https://api.thedogapi.com/v1/images/search`
+
+
+//fetching json
+
+const App = () => { 
+  const [photos, setPhotos] = useState ([])
+    useEffect(() => {
+    fetch(url, { headers: { Authorization: apiKey} })
+      .then(res => res.json())
+      .then(json => {
+      setPhotos(json)
+        console.log(json)
+  })
+  }, [])
+
+    const onPress = (photo) => {
+    console.log('click click', photo.url)
+  }
+
+    return(
+        <Container>
+            <View>
+             {photos.map((photo) => (
+
+             <View key={photo.id}> 
+             <Image
+               source={{uri: photo.url}}
+               style={{width: 300, height: 600, resizeMode: "contain"}} 
+              />
+           </View>
+         ))}
+
+            <TouchableOpacity onPress={(photo) => onPress(photo)}> 
+              <Text>New dog</Text>
+            </TouchableOpacity> 
+        </View>  
+      </Container>
+    );
+  }
+
+  export default App
+
+
+  const Button = styled.TouchableOpacity`
+    width: 200;
+    position: absolute;
+    border-radius: 5%;
+    margin-bottom: 200px;`
+
+  const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
   align-items: center;
-`
-
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
-
-export default App
+  justify-content: center;
+  text-align: center;
+  background-color: black;
+  padding_top: 200;
+  padding_bottom: 200;
+  `
+  const Label = styled.View`
+  color: white;`
