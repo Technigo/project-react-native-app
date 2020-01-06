e4b1f6eec1b9d6846d1e7fb35ae21bc202e26d7e
sarah wolfson <swolfson23@gmail.com>
2020-01-06 14:26:06 +0100

    api works

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components/native"
 
-const App = () => {
+const apiKey = `const apiKey = 'K3UmmZiUOJcE1TWabtU7ygeF`;
+const url = `https://api.fungenerators.com/taunt/generate?category=pirate-insult`;
+
+export const App = () => {
+  const [insult, setInsult] = useState([]);
+
+
+  useEffect(() => {
+    fetch(url, { headers: { Authorization: apiKey } })
+      .then(res => res.json())
+      .then(json => {
+        setInsult(json.insult);
+        console.log(json);
+      });
+  }, []);
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
+      <Title>taunt</Title>
       <Title>Go to App.js and start coding</Title>
       <Title>💅💅💅</Title>
     </Container>
