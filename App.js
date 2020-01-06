ad55328d8f079c07660170c12ce563e9e3be2438
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-01-06 18:06:53 +0100

    share function works and linking to the photgrapher site

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState, Component } from "react";
 import Header from "./components/Header"
-// import Button from "./components/Button"
 import styled from "styled-components"
-import { Image, View, TouchableOpacity, Text, Title, ScrollView, Linking } from 'react-native';
+import { 
+  Image, 
+  View, 
+  TouchableOpacity, 
+  Text, 
+  ScrollView, 
+  Linking, 
+  Share 
+} from 'react-native';
 
 
 const apiKey = `563492ad6f917000010000016674d16c530e444482c459f1837b2a47`
@@ -10,6 +17,7 @@ const url = "https://api.pexels.com/v1/search?query=example+crossfit&per_page=66
 
 const App = () => {
   const [photos, setPhotos] = useState([])
+  
   useEffect(() => {
     fetch(url, { headers: { Authorization: apiKey } })
       .then(res => res.json())
@@ -19,40 +27,81 @@ const App = () => {
       })
   }, [])
 
-  const getSelectedImageUrl = (photoId) => {
-    const photo = photos.find((p) => p.id === photoId)
-    return Linking.openURL(photo.photographer_url)
+  const getImageToShare = photoId => {
+    const photo = photos.find(p => p.id === photoId)
+    return photo.src.original
   }
+
+  const onShare = async photoId => {
+    const result = await Share.share({
+      message: 'Hi there, isnt this just awesome?!',
+      url: getImageToShare(photoId),
+      title: 'CrossFit power coming up!'
+    })
   
+    if (result.action === Share.sharedAction) {
+      if (result.activityType) {
+        // shared with activity type of result.activityType
+        alert('Some strength just shared!')
+      } else {
+        // shared
+      }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+        alert('dismissed')
+      }
+   }
+
+   const getSelectedImageUrl = (photoId) => {
+     const photo = photos.find((p) => p.id === photoId)
+     return Linking.openURL(photo.photographer_url)
+   }
+
   return(
-    <Container><ScrollView>
-      <Header title='Rx CrossFit Snaps' />
-      {photos.map((photo) => (
-        <View key={photo.id}>
-          <Button onPress={() => getSelectedImageUrl(photo.id)}>
-            <Image
-              source={{uri: photo.src.original}}
-              style={{width:400, height:300, marginVertical: 10, }}
-            />
-          </Button>
-          <Text
-            style={{color:'white', }}>Photo: {photo.photographer}
-          </Text>
-        </View>
-      ))
-      }   
-    </ScrollView></Container >
+    <Container>
+      <Header title='Rx CrossFit Power' />
+        <ScrollView>
+          {photos.map((photo) => (
+            <View key={photo.id}>
+            
+              <TouchableOpacity onPress={() => getSelectedImageUrl(photo.id)}>
+                <Image
+                  source={{uri: photo.src.original}}
+                  style={{width:400, height:300, marginVertical: 10, }}
+                />
+              </TouchableOpacity>
+              
+              <Text
+                style={{color:'white', textAlign: 'center' }}>Photo: {photo.photographer}
+              </Text>
+              
+              <View>
+                <Button onPress={() => onShare(photo.id)} title="Share">
+                  <Text
+                    style={{color:'white' }}>
+                    Share some RX Power!
+                  </Text>
+                </Button>
+              </View>
+            </View>
+        ))
+        }   
+      </ScrollView>
+    </Container >
   )  
 }
 
 export default App
 
+
 const Button = styled.TouchableOpacity`
-  width: 400;
-  height: 300;
-  background-color: black;
-  margin-bottom: 20;
-  border-radius: 5;
+  position: absolute;
+  bottom: 28px;
+  background: black;
+  padding: 10px 5px;
+  border-radius: 20px;
+  text-align: center;
+  left: 31%;
 
 ` 
 
@@ -60,14 +109,8 @@ const Container = styled.View`
   flex: 1;  
   background-color: black; 
   justifyContent: space-between; 
-  alignContent: center;
+  
   paddingHorizontal: 20;
   paddingVertical: 20;
-  flexWrap: wrap;
-    
+
 `
-
- 
-
-
-
