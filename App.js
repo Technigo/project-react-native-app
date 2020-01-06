1e00124cbe708a3439ac31fb9b95b4db1af29dd0
tavferreira <tiagoferreira@tuta.io>
2020-01-06 23:27:57 +0100

    Grid rendered and some play logic added

--- App.js
+++ App.js
@@ -1,26 +1,85 @@
-import React from "react"
-import styled from "styled-components/native"
+import React, { useState } from "react"
+import { View, Alert } from "react-native"
+import styled from 'styled-components/native'
+import { Row } from "./components/Row"
+import { Tile } from "./components/Tile"
+import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons'
+
 
 const App = () => {
+  const [grid,setGrid] = useState([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ])
+  const [player,setPlayer] = useState(1)
+  const [moves,setMoves] = useState(1)
+
+  const renderIcon = (x,y) => {
+    switch(grid[x][y])
+    {
+      case 1: return <Icon name="checkbox-blank-circle-outline" size={50} color="darkred"/>;
+      case -1: return <Icon name="close"  size={50} color="orange"/>;
+      default: return <View />;
+    }
+  }
+
+  const playerMove = (x,y) => {
+    if(moves < 10 && grid[x][y] === 0) {
+      let gridCopy = grid.slice()
+
+      gridCopy[x][y] = player
+      setGrid(gridCopy)
+      setPlayer(player*-1)
+      setMoves(moves+1)
+    }
+  }
+
   return (
-    <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
+    <Grid>
+      <Row>
+        <Tile onPress={() => playerMove(0,0)}>
+          {renderIcon(0,0)}
+        </Tile>
+        <Tile onPress={() => playerMove(0,1)}>
+          {renderIcon(0,1)}
+        </Tile>
+        <Tile onPress={() => playerMove(0,2)}>
+          {renderIcon(0,2)}
+        </Tile>
+      </Row>
+      <Row>
+        <Tile onPress={() => playerMove(1,0)}>
+          {renderIcon(1,0)}
+        </Tile>
+        <Tile onPress={() => playerMove(1,1)}>
+          {renderIcon(1,1)}
+        </Tile>
+        <Tile onPress={() => playerMove(1,2)}>
+          {renderIcon(1,2)}
+        </Tile>
+      </Row>
+      <Row>
+        <Tile onPress={() => playerMove(2,0)}>
+          {renderIcon(2,0)}
+        </Tile>
+        <Tile onPress={() => playerMove(2,1)}>
+          {renderIcon(2,1)}
+        </Tile>
+        <Tile onPress={() => playerMove(2,2)}>
+          {renderIcon(2,2)}
+        </Tile>
+      </Row>
+    </Grid>
   )
 }
 
-const Container = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
-  align-items: center;
-`
-
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
-
 export default App
+
+const Grid = styled.View`
+    display: flex;
+    background-color: #fff;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+`
---
+++ components/Tile.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+
+export const Tile = styled.TouchableOpacity`
+    border: 2px;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
