5f455f49a31569a7667cebe8be849b2869cae17b
ucha3i <jchabik@gmail.com>
2020-01-07 14:09:31 +0100

    first version

--- App.js
+++ App.js
@@ -1,70 +1,82 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components/native'
-import { ActivityIndicator } from 'react-native'
-import { Movie } from './components/Movie'
+import React, { useState, useEffect } from "react"
+import styled from "styled-components/native"
+import { DeviceMotion } from "expo-sensors"
 
-const TOP_MOVIE_ID = 656516
-
-const Container = styled.View`
-  flex: 1;
-  background-color: black;
-  align-items: center;
-  justify-content: center;
-`
-
-const Button = styled.TouchableOpacity`
-  background: #80ff86;
-  position: absolute;
-  bottom: 40;
-  padding: 10px 20px;
-  border-radius: 20px;
-`
-
-const ButtonText = styled.Text`
-  color: #1f2e1f;
-  font-size: 18;
-`
-
-const fetchRandomMovie = async () => {
-  const movieId = Math.floor(Math.random() * TOP_MOVIE_ID) + 1
-  const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=6420add5c0a9b0e0b9462a92916c3187&language=en-US`)
-  const json = await res.json()
-
-  if (json.status_code === 34 || !json.poster_path || json.adult) {
-    return fetchRandomMovie()
-  } else {
-    return json
-  }
-}
+const songs = [
+  "AC/DC - Thunderstruck",
+  "Katy Perry - Last Friday Night",
+  "The Killers - Mr. Brightside",
+  "Taylor Swift - Shake it off",
+  "Billie Eilish - Bad Guy",
+  "Sam Cooke - Wonderful World",
+  "Frank Sinatra - My way",
+  "The Beatles - Hey Jude",
+  "Madonna - Like a Virgin",
+  "Abba - Dancing Queen",
+  "Avicii - Wake up",
+  "Imagine Dragons - Thunder",
+  "George Ezra - Budapest",
+  "Counting Crows - Accidentally in Love", 
+  ""
+]
 
 const App = () => {
-  const [movie, setMovie] = useState()
-  const [loading, setLoading] = useState(true)
+  const [song, setSong] = useState()
 
-  const fetchMovieData = () => {
-    setLoading(true)
-    fetchRandomMovie()
-      .then((movieData) => {
-        setMovie(movieData)
-        setLoading(false)
-      })
+  const showRandomSong = () => {
+    console.log("bytt")
+    const randomIndex = Math.round(Math.random() * (songs.length - 1))
+    setSong(songs[randomIndex])
   }
-
-  useEffect(() => {
-    fetchMovieData()
-  }, [])
 
   return (
     <Container>
-      {loading
-        ? <ActivityIndicator size="large" color="#ffffff" />
-        : <Movie movie={movie} />}
-
-      <Button onPress={fetchMovieData}>
-        <ButtonText>Fetch new movie</ButtonText>
+      <Image resizeMode='contain' source={require("./assets/note.png")} />
+      <Title>
+        {song ||
+          "No idea what to listen today? Press the magic music button and get your daily song!"}
+      </Title>
+      {/* <Subtitle>Spin your phone to get a song!</Subtitle> */}
+      <Button onPress={showRandomSong}>
+        <H1>Magic music button</H1>
       </Button>
     </Container>
   )
 }
+
+const Container = styled.View`
+  flex: 1;
+  background-color: purple;
+  justify-content: center;
+  align-items: center;
+`
+
+const Image = styled.Image`
+  height: 330;
+  margin-bottom: 70;
+`
+
+const Title = styled.Text`
+  font-size: 21px;
+  color: white;
+  margin-bottom: 20;
+  text-align: center;
+  padding: 0 15px;
+`
+
+const Button = styled.TouchableOpacity`
+  background-color: pink;
+  border-radius: 50;
+  text-align: center;
+  padding: 10px 20px;
+  margin-top: 30;
+`
+
+const H1 = styled.Text`
+  font-size: 20px;
+  color: white;
+  padding: 10px 50px;
+  text-align: center;
+`
 
 export default App
