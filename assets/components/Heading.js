eb5ee0640443f061c17806c0669d38b11dcdcec7
Emmi Nilsson <emmie.nilsson@gmail.com>
2020-01-07 10:46:24 +0100

    Pokemon randomizer.

---
+++ assets/components/Heading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+
+export const Heading = styled.Text`
+  color: #4b244a;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
