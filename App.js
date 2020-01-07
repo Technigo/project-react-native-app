e8894e83d2168c5926fe6945836d25d9435a75c7
Frida Jonsson <annafridamariajonsson@gmail.com>
2020-01-07 19:53:34 +0100

    Fetch & display character, buttons

--- App.js
+++ App.js
@@ -1,90 +1,138 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import { View, Text, Button } from 'react-native'
 import styled from 'styled-components/native'
+import Character from './components/Character'
 
-const Button = styled.TouchableOpacity`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 70%;
-  height: 60px;
-  top: 80;
-  padding: 10px 20px;
-  border-radius: 20px;
-  background: white;
-  color: black;
-  border: solid 3px black;
-  font-size: 40px;
-  `
+// const Button = styled.TouchableOpacity`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   width: 70%;
+//   height: 60px;
+//   top: 80;
+//   padding: 10px 20px;
+//   border-radius: 20px;
+//   background: white;
+//   color: black;
+//   border: solid 3px black;
+//   font-size: 40px;
+//   `
   const Container = styled.View`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: #391F5C;
-  position: absolute;
+  background: white;
   height: 100%;
   width: 100%;
+  color: red;
   `
 
-const App = () => {
-  // const [character, setCharacter] = useState()
-  // const [loading, setLoading] = useState(true)
+  const Header = styled.Text`
+  font-size: 30px;
+  color: black;
+  `
 
-  // const fetchCharacterData = () => {
-  //   setLoading(true)
-  //   fetchRandomCharacter()
-  //     .then((fetchCharacterData) => {
-  //       setCharacter(fetchCharacterData)
-  //       setLoading(false)
-  //     })
-  // }
-
-  // useEffect(() => {
-  //   fetchCharacterData()
-  // }, [])
-
-  const fetchRandomCharacter = () => {
-    const characterIndex = (param) => {
-      return Math.floor(Math.random() * param.length) - 1
-    }
-    const apiKey = "$2a$10$QO0C49uSavMxC6YqmS5me.3sTZykI3k22Hn8I8zO4t3LaAdmAzFBm"
-    const url = "https://www.potterapi.com/v1/"
-     
-      fetch(`${url}characters/?key=${apiKey}`)
-      .then((res) => res.json())
-      .then((json) => {
-          let characterId = characterIndex(json)
-          let character = (json[characterId])
-          if (!character.house) {
-            return fetchRandomCharacter()
-          } else {
-            return (
-              <Container>
-                <Text>
-                  <h2>${character.name}</h2> 
-                  <h4>${character.house}</h4>
-                </Text>
-              </Container>
-            )
-          }
-      })
+  const POTTER_API_URL = 'https://www.potterapi.com/v1'
+  const POTTER_API_KEY =
+    '$2a$10$QO0C49uSavMxC6YqmS5me.3sTZykI3k22Hn8I8zO4t3LaAdmAzFBm'
+  
+  function fetchCharacters() {
+    return fetch(`${POTTER_API_URL}/characters?key=${POTTER_API_KEY}`).then(
+      response => response.json(),
+    )
   }
-
-  return (
-    <Container>
-      {/* {loading
-        ? <ActivityIndicator size="large" color="#ffffff" />
-        : <Movie movie={movie} />} */}
-
+  
+  function randomRangeNumber(max) {
+    return Math.floor(Math.random() * max)
+  }
+  
+  function getRandomCharacter(characters) {
+    let character = characters[randomRangeNumber(characters.length) - 1]
+  
+    while (!character.house) {
+      character = characters[randomRangeNumber(characters.length) - 1]
+    }
+  
+    return character
+  }
+  
+  function getRandomCharacterRecursion(characters) {
+    let character = characters[randomRangeNumber(characters.length) - 1]
+  
+    // stop condition
+    if (!character.house) {
+      return getRandomCharacterRecursion(characters)
+    }
+  
+    return character
+  }
+  
+  export default function App() {
+    const [character, setCharacter] = useState()
+  
+    const handleFetchCharacters = () => {
+      fetchCharacters().then(json => {
+        // recursion
+        // let character = getRandomCharacterRecursion(json)
+        // no recursion
+        let character = getRandomCharacter(json)
+        setCharacter(character)
+      })
+    }
+  
+    useEffect(() => {
+      handleFetchCharacters()
+    }, [])
+  
+    if (!character) {
+      return null
+    }
+  
+    return (
+      <Container>
+        <Header>
+          {character.name}
+        </Header>
       <Button
-      onPress={fetchRandomCharacter}
-      title="Random character">
-        <Text>
-        Random character
-        </Text>
+        title="Gryffindor"
+        onPress={() => {
+          console.log(character.house === 'Gryffindor')
+        }}
+      >
+        Gryffindor
       </Button>
-    </Container>
-  )
-}
-
-export default App
+      <Button
+        title="Slytherin"
+        onPress={() => {
+          console.log(character.house === 'Slytherin')
+        }}
+      >
+        Slytherin
+      </Button>
+      <Button
+        title="Hufflepuff"
+        onPress={() => {
+          console.log(character.house === 'Hufflepuff')
+        }}
+      >
+        Hufflepuff
+      </Button>
+      <Button
+        title="Ravenclaw"
+        onPress={() => {
+          console.log(character.house === 'Ravenclaw')
+        }}
+      >
+        Ravenclaw
+      </Button>
+      <Button
+        title="random"
+        onPress={() => {
+          handleFetchCharacters()
+        }}
+      >
+        Random
+      </Button>
+      </Container>
+    )
+  }
---
+++ components/Character.js
@@ -0,0 +1,18 @@
+import React, {Text, Container} from 'react'
+import styled from 'styled-components/native'
+
+// const Container = styled.SafeAreaView`
+//   flex: 0.5;
+//   width: 100%;
+//   height: 100%;
+//   align-items: center;
+//   background: blue;
+//   `
+
+export const Character = () => (
+  <Container>
+    <Text>
+        Hello world
+    </Text>
+  </Container>
+)
