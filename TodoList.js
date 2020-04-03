6e74d0a97e8acc53bc61679cf459c2e4553f334e
Frida Eriksson <Frida.Eriksson@silf.se>
2020-04-03 23:33:53 +0200

    added toggleCompleted function to toggle styling of checkmark and todo text, using ternary op for checkmark and props to conditionally style text

--- TodoList.js
+++ TodoList.js
@@ -1,20 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/native'
+import { Text, TouchableOpacity, Dimensions } from 'react-native'
 
 export const TodoList = () => {
+  const [completed, setCompleted] = useState(false)
+  const [textCompleted, setTextCompleted] = useState(false)
+
+  const toggleCompleted = () => {
+    if (completed) {
+      setCompleted(false)
+      setTextCompleted(false)
+    } else {
+      setCompleted(true)
+      setTextCompleted(true)
+    }
+  }
+
   return (
     <Container>
-      <Todo>GET SH#T DONE 2</Todo>
+      <TodoContainer>
+        <TouchableOpacity onPress={toggleCompleted}>
+          {completed ? <CircleComplete><CheckMark>&#10003;</CheckMark></CircleComplete> : <CircleNotComplete />}
+        </TouchableOpacity>
+        <Todo textCompleted={textCompleted}>Placeholder</Todo>
+      </TodoContainer>
     </Container>
   )
 }
 
 const Container = styled.ScrollView`
+  padding: 20px;
   color: #000;
 `
 
-const Todo = styled.Text`
-  padding: 20px;
+const TodoContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+const CircleComplete = styled.View`
+  width: 25px;
+  height: 25px;
+  border-radius: 15px;
+  border-color: #2c3e50;
+  background: #2c3e50;
+  border-width: 2px;
+  margin-right: 20px;
+  color: #fff
+`
+
+const CheckMark = styled.Text`
   font-size: 16px;
-  color: #000;
+  text-align: center;
+  color: #fff;
+`
+
+const CircleNotComplete = styled.View`
+  width: 25px;
+  height: 25px;
+  border-radius: 15px;
+  border-color: #2c3e50;
+  border-width: 2px;
+  margin-right: 20px;
+`
+
+const Todo = styled.Text`
+  font-size: 16px;
+  ${prop => prop.textCompleted ? 'color: #bbb; text-decoration: line-through; text-decoration-color: #bbb' : 'color: #2c3e50'};
 `
