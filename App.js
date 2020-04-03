feb0b34ff2185acef5012d7d883d9c09bc926918
Frida Eriksson <Frida.Eriksson@silf.se>
2020-04-03 21:27:42 +0200

    added View and TextInput with some basic styling

--- App.js
+++ App.js
@@ -1,24 +1,44 @@
 import React from 'react'
 import styled from 'styled-components/native'
+import { View, Text, TextInput } from 'react-native'
 
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
+  background-color: #2c3e50;
+  justify-content: flex-start;
   align-items: center;
+  
 `
 
 const Title = styled.Text`
+  padding-top: 40px;
+  padding-bottom: 20px;
   font-size: 24px;
-  color: palevioletred;
+  color: #fff;
 `
+
+const ViewCard = styled.View`
+  flex: 1;
+  width: 90%;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  background: #fff;
+`
+
+const InputField = styled.TextInput`
+  padding: 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: #bbb;
+  font-size: 16px;
+  `
 
 const App = () => {
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
+      <Title>GET SH#T DONE</Title>
+      <ViewCard>
+        <InputField placeholder="Add task" />
+      </ViewCard>
     </Container>
   )
 }
