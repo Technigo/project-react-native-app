6e30e990c184039a2f71b78732422fc404d91300
Mathias Thorström <thisen@gmail.com>
2020-04-06 20:28:32 +0200

    converted unixtime with moment and added scrollview component

--- component/DetailScreen.js
+++ component/DetailScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components/native'
+import Moment from 'react-moment';
 import {View, Text, Button} from 'react-native'
 
 export const DetailScreen = ({ route, navigation }) => {
@@ -9,8 +10,14 @@ export const DetailScreen = ({ route, navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-      <Text>{launch.mission_name}</Text>
-      <Text>{launch.launch_year}</Text>
+      <Text>Misison Name: {launch.mission_name}</Text>
+      {/* <Text>Launch date: {launch.launch_date_unix}</Text> */}
+      <Text>Launch Date: 
+        <Moment unix format="YYYY/MM/DD" element={Text}>{launch.launch_date_unix}</Moment>
+      </Text>
+      <Text>Rocket Name: {launch.rocket.rocket_name}</Text>
+      <Text>Type: {launch.rocket.second_stage.payloads[0].payload_type}</Text>
+
       <Button
         title="Go to Home"
         onPress={() => navigation.navigate('Home')}
--- component/LaunchPadScreen.js
+++ component/LaunchPadScreen.js
@@ -1,15 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import styled from 'styled-components/native'
-import {View, Text, ActivityIndicator, Button} from 'react-native'
+import {View, Text, ActivityIndicator, ScrollView} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
+  background-color: #FFF;
   justify-content: center;
   align-items: center;
+`
+const Title = styled.Text`
+  font-size: 24px;
+  color: red;
+  padding: 10px;
+  background-color: purple;
 `
 
 function LoadingScreen () {
@@ -32,22 +38,25 @@ export const LaunchPadScreen = ({ navigation }) => {
   }, [])
 
   return (
-    <Container>
+    <ScrollView>
+      <Container>
 
-      {/* Loading anitmation if fetch response takes time */}
-      {isLoading && <LoadingScreen />}
+        {/* Loading anitmation if fetch response takes time */}
+        {isLoading && <LoadingScreen />}
 
-      {launches.map((launch) => (
-        // Creates a button that will navigate to Detail page and pass all data on that launch into Detail to be used.
-        // Style this touchableOpacity or better element to be my display card for each launch
-        <TouchableOpacity
-        key={launch.mission_name}
-        onPress={() => navigation.navigate('Detail', { launch })} // navigating to details screen with the clicked house info passed along
-        >
-          <Text key={launch.mission_name}>{launch.mission_name}</Text>
-        </TouchableOpacity>
-      ))}
+        {launches.map((launch) => (
+          // Creates a button that will navigate to Detail page and pass all data on that launch into Detail to be used.
+          // Style this touchableOpacity or better element to be my display card for each launch
+          <TouchableOpacity
+          key={launch.mission_name}
+          onPress={() => navigation.navigate('Detail', { launch })} // navigating to details screen with the clicked house info passed along
+          >
+            <Title key={launch.mission_name}>{launch.mission_name}</Title>
+          </TouchableOpacity>
 
-    </Container>
+        ))}
+
+      </Container>
+    </ScrollView>
   )
 }
