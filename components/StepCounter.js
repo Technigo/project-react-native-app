4869f6c45be442b5f79164036df929aa102d8345
annatakman <annatakman@gmail.com>
2020-04-07 12:00:40 +0200

    added week step count

--- components/StepCounter.js
+++ components/StepCounter.js
@@ -16,10 +16,15 @@ align-items: center;
 const StepCounter = () => {
   const [currentStepCount, setCurrentStepCount] = useState(0)
   const [pastStepCount, setPastStepCount] = useState(0)
+  const [weekStepCount, setWeekStepCount] = useState(0)
 
   const end = new Date();
   const start = new Date();
   start.setDate(end.getDate() - 1);
+
+  const weekEnd = new Date();
+  const weekStart = new Date();
+  weekStart.setDate(weekEnd.getDate() - 7);
 
   useEffect(() => {
     Pedometer.watchStepCount(result => {
@@ -29,11 +34,16 @@ const StepCounter = () => {
       .then(result => {
         setPastStepCount(result.steps);
       })
+    Pedometer.getStepCountAsync(weekStart, weekEnd)
+      .then(result => {
+        setWeekStepCount(result.steps);
+      })
   })
 
   return (
     <Container>
-      <Title>Steps taken in the last 24 hours: {pastStepCount} steps</Title>
+      <Title>Steps taken the last week: {weekStepCount}</Title>
+      <Title>Steps taken in the last 24 hours: {pastStepCount}</Title>
       <Title>Walk and watch this go up: {currentStepCount}</Title>
     </Container>
   )
