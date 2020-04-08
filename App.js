7f4e91a6eba8be367c0126d1d011185a91bb4501
sara <sara.lovens.lovn@gmail.com>
2020-04-08 10:13:28 +0200

    minor fixing

--- App.js
+++ App.js
@@ -1,52 +1,48 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import { StyleSheet, View, ScrollView, ImageBackground, Button } from 'react-native';
-import styled from 'styled-components/native'
-import {Weather} from './components/Weather';
-import {Compass} from './components/Compass';
-import {Heading} from './components/Heading';
+import styled from 'styled-components/native';
+import { Weather } from './components/Weather';
+import { Compass } from './components/Compass';
+import { Heading } from './components/Heading';
 import MyNewLocation from './components/MyNewLocation';
 import MyLocation from './components/MyLocation';
 
 const Container = styled.View`
-  flex: 1;
-  background-color: black;
-  justify-content: center;
-  align-items: center;
-`
+	flex: 1;
+	background-color: black;
+	justify-content: center;
+	align-items: center;
+`;
 
 const App = () => {
-  [myCord, setMyCord] = useState({latitude: 0, longitude: 0})
-  const [count, setCount] = useState(0)
-  return (
-    <Container>
-    <ScrollView contentContainerStyle = {styles.children} >
-    <ImageBackground source={require('./assets/ForestBackground.jpg')} style={styles.image} >
-     <Heading />
-     <MyNewLocation myCord = {myCord} setMyCord = {setMyCord} count={count} />
-     <Compass />
-    {(myCord.latitude !== 0 || myCord.longitude !== 0 ) && <Weather myCord = {myCord} />}
-    <Button onPress={()=>setCount(count+1)} title = "I have moved" color="darkolivegreen" />
-  </ImageBackground>
-  </ScrollView>
-  </Container>
-  )
-}
+	const [ myCord, setMyCord ] = useState({ latitude: 0, longitude: 0 });
+	const [ count, setCount ] = useState(0);
+	return (
+		<Container>
+			<ScrollView contentContainerStyle={styles.children}>
+				<ImageBackground source={require('./assets/ForestBackground.jpg')} style={styles.image}>
+					<Heading />
+					<MyNewLocation myCord={myCord} setMyCord={setMyCord} count={count} />
+					<Compass />
+					{(myCord.latitude !== 0 || myCord.longitude !== 0) && <Weather myCord={myCord} />}
+				</ImageBackground>
+			</ScrollView>
+		</Container>
+	);
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
-
-  children: {
-    height: 1500,
-    width: 400,
-    flexGrow: 1,
-    justifyContent: 'center',
-
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    
-  },
+	children: {
+		height: 1500,
+		width: 400,
+		flexGrow: 1,
+		justifyContent: 'center'
+	},
+	image: {
+		flex: 1,
+		resizeMode: 'cover',
+		justifyContent: 'center'
+	}
 });
--- Components/MyNewLocation.js
+++ Components/MyNewLocation.js
@@ -1,89 +1,87 @@
 import React, { useState, useEffect } from 'react';
 import { Platform, Text, View, StyleSheet, Button } from 'react-native';
-import styled from "styled-components/native"
+import styled from 'styled-components/native';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={your api key}
 const Container = styled.View`
-display: flex;
-flex-direction: row;
-background-color: white;
-opacity: 0.7;
-align-items: center;
-justify-content: space-around;
-border-width: 1px;
-border-color: black;
-
+	display: flex;
+	flex-direction: row;
+	background-color: white;
+	opacity: 0.7;
+	align-items: center;
+	justify-content: space-around;
+	border-width: 1px;
+	border-color: black;
 `;
 
-const MyNewLocation = ({myCord, setMyCord, count}) => {
-  const [state, setState] = useState({
-    location: null,
-    errorMessage: null,
-  })
-  const [long, setLong ] = useState(0)
-  const [lat, setLat] = useState(0)
+const MyNewLocation = ({ myCord, setMyCord, count }) => {
+	const [ state, setState ] = useState({
+		location: null,
+		errorMessage: null
+	});
+	const [ long, setLong ] = useState(0);
+	const [ lat, setLat ] = useState(0);
 
-  useEffect(() => {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-      setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-      });
-    } else {
-      _getLocationAsync();
-    }
-  }, [count])
+	useEffect(() => {
+		if (Platform.OS === 'android' && !Constants.isDevice) {
+			setState({
+				errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
+			});
+		} else {
+			_getLocationAsync();
+		}
+	}, []);
 
-  const _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      setState({
-        errorMessage: 'Permission to access location was denied',
-      });
-    }
+	const _getLocationAsync = async () => {
+		let { status } = await Permissions.askAsync(Permissions.LOCATION);
+		if (status !== 'granted') {
+			setState({
+				errorMessage: 'Permission to access location was denied'
+			});
+		}
 
-    let geocode = await Location.getCurrentPositionAsync({});
-    let location = await Location.reverseGeocodeAsync(geocode.coords);
-    setState({ location })
-    setLong(geocode.coords.longitude)
-    setLat(geocode.coords.latitude)
-    setMyCord({latitude: Math.round(geocode.coords.latitude), longitude: Math.round(geocode.coords.longitude)})
-    ;
-  };
- 
-  let text2 = ''
-  let text = 'Waiting..';
-  if (state.errorMessage) {
-    text = state.errorMessage;
-  } else if (state.location) {
-    // text = JSON.stringify(state.location);
-    text = `You are at ${state.location[0].region}!`
-    text2 = `You are at ${Math.round(myCord.latitude)} ${Math.round(myCord.longitude)}!`
-    console.log("state", state.location)
-  }
+		let geocode = await Location.getCurrentPositionAsync({});
+		let location = await Location.reverseGeocodeAsync(geocode.coords);
+		setState({ location });
+		setLong(geocode.coords.longitude);
+		setLat(geocode.coords.latitude);
+		setMyCord({ latitude: Math.round(geocode.coords.latitude), longitude: Math.round(geocode.coords.longitude) });
+	};
 
-  return (
-    <Container>
-      <Text style={styles.paragraph}>{text}</Text>
-    </Container>
-  );
-}
+	let text2 = '';
+	let text = 'Waiting..';
+	if (state.errorMessage) {
+		text = state.errorMessage;
+	} else if (state.location) {
+		// text = JSON.stringify(state.location);
+		text = `You are at ${state.location[0].region}!`;
+		text2 = `You are at ${myCord.latitude} ${myCord.longitude}!`;
+		console.log('state', state.location);
+	}
+
+	return (
+		<Container>
+			<Text style={styles.paragraph}>{text}</Text>
+		</Container>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    textAlign: 'center',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingTop: Constants.statusBarHeight,
+		backgroundColor: '#ecf0f1'
+	},
+	paragraph: {
+		margin: 24,
+		fontSize: 18,
+		textAlign: 'center'
+	}
 });
 
 export default MyNewLocation;
