ef84ed8236c3468ad8dfa940494a597c0f9d8e7a
palhamel <pal.hamel@gmail.com>
2020-04-10 10:18:17 +0200

    Add new foundation

--- App.js
+++ App.js
@@ -1,30 +1,56 @@
 import React from 'react'
-import styled from 'styled-components/native'
-import { Houses } from './components/Houses'
+// import styled from 'styled-components/native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png'
 
 
-const Container = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
-  align-items: center;
-`
 
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
-
-const App = () => {
+export default function App() {
   return (
-    <Container>
-      <Houses />
-      
-    </Container>
-  )
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} /> 
+      <Text style={{color: '#888', fontSize: 18}}> 
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+
+
+    </View>
+  );
 }
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }, 
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
+});
 
 
 
