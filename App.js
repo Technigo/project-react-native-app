ef14109e6a179ad913cb038c18376ee4a02b6080
ebbabw <ebbabergholtz@gmail.com>
2020-04-10 16:17:57 +0200

    Added navigation andfixed the restaurants.js

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
+import 'react-native-gesture-handler';
 import React from 'react'
 import styled from 'styled-components/native'
-import { ListPage } from './ListPage'
+import { Restaurants } from './Pages/Restaurants'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Container = styled.View`
   flex: 1;
-  background-color: blue;
-  justify-content: center;
-  align-items: center;
+  background-color: white;
 `
 
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <Container>
-      <ListPage />
+    <NavigationContainer>
+      <Container>
+    <Stack.Navigator>
+      <Stack.Screen name="Restaurants" component={Restaurants} />
+    </Stack.Navigator>
     </Container>
+  </NavigationContainer>
   )
 }
 
---
+++ Pages/Restaurants.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import { FlatList } from 'react-native';
+import styled from 'styled-components/native'
+
+
+const apiKey  = "494da9ce0d7d396b3dfc1e2b322b10bd";
+
+
+export const Restaurants = () => {
+
+    const [foodplaces, setFoodplaces] = useState([])
+
+    useEffect(() => {
+      fetch( `https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city`, {headers:{ "user-key": apiKey}})
+      .then((res) => res.json())
+      .then((json) => {
+        setFoodplaces(json.restaurants)
+        console.log(json.restaurants)
+      })
+      .catch((error) => console.log(error))
+    }, [])
+
+    return (
+
+      <Container>
+
+      <PageTitle>Trending this Week</PageTitle>
+      <SubTitle>New York City</SubTitle>
+      
+
+    
+      <FlatList 
+
+      horizontal={true}
+      data={foodplaces}
+      renderItem={({ item }) => (
+
+       
+        <ItemTContainer>
+
+        <Itemtext>{item.restaurant.name}</Itemtext>
+        <Itemtext>{item.restaurant.location.city}</Itemtext>
+        
+        </ItemTContainer>
+     
+      )}
+      keyExtractor={item => item.restaurant.id}
+      
+      />
+      
+      </Container>
+
+    )
+   
+ }
+
+const Container = styled.View`
+  background-color: white;
+  padding-top: 30px;
+`
+const PageTitle = styled.Text`
+  margin-top: 60px;
+  margin-left: 27px;
+  margin-bottom: 20px;
+  font-size: 24px;
+  color: black;
+`
+const SubTitle = styled.Text`
+  margin-left: 30px;
+  font-size: 24px;
+  color: black;
+`
+const ItemTContainer= styled.View`
+  margin: 24px;
+  padding: 50px;
+  border: 2px solid black;
+  height: 300px;
+`
+const Itemtext = styled.Text`
+  margin-top: 20px;
+  font-size: 24px;
+  color: palevioletred;
+`
+
