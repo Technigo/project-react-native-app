09cf88fd77a8d7d6357b34dbaba05c3b4ce119c9
Olle Alm <olle.alm@me.com>
2020-04-13 21:36:29 +0200

    styled components

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { Button, TouchableOpacity, Text, View, StyleSheet, Alert } from "react-native";
 import { AppLoading } from 'expo'
 import { useFonts } from '@use-expo/font';
 import QuoteContainer from './components/QuoteContainer';
 import styled from 'styled-components/native'
-
 
 const Container = styled.View`
   flex: 1;
@@ -12,37 +10,23 @@ const Container = styled.View`
   justify-content: center;
   align-items: center;
 `
-
-const textStyle = styled.Text`
-  font-size: 20px;
-  font-family: 'Introspect-Bk';
-  color: white;
+const ClickSurface = styled.TouchableOpacity`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  alignItems: center;
+  backgroundColor: transparent;
 `
-
-const styles = StyleSheet.create({
-  button: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    backgroundColor: "transparent",
-    padding: 10
-  },
-});
-
 
 const App = () => {
   const [quoteArray, setQuoteArray] = useState([])
   const [quote, setQuote] = useState("test")
-  const onPress = () => {
-    setQuote(randomQuote(quoteArray));
-  }
-
-  const url = 'https://raw.githubusercontent.com/keypressingmonkey/stoicquotesapp/master/JSONFIles/quotes.json'
 
   let [fontLoaded] = useFonts({
     'Introspect-Bk': require('./assets/fonts/Introspect-Bk.otf'),
   })
+
+  const url = 'https://raw.githubusercontent.com/keypressingmonkey/stoicquotesapp/master/JSONFIles/quotes.json'
 
   useEffect(() => {
     fetch(url)
@@ -53,18 +37,19 @@ const App = () => {
       })
   }, [])
 
+  const onPress = () => {
+    setQuote(randomQuote(quoteArray));
+  }
+
   if (!fontLoaded || !quote) {
     return <AppLoading />
   } else {
     return (
       <Container>
-        <QuoteContainer text={quote} textStyle={textStyle} />
-
-        <TouchableOpacity
-          style={styles.button}
+        <QuoteContainer quote={quote} />
+        <ClickSurface
           onPress={onPress}
-        >
-        </TouchableOpacity>
+        />
       </Container>
     )
   }
@@ -74,7 +59,7 @@ export default App
 
 const randomQuote = (array) => {
   let quote = array[Math.floor(Math.random() * array.length)].quote
-  while (quote.length > 300) {
+  while (quote.length > 210) {
     quote = array[Math.floor(Math.random() * array.length)].quote
   }
   return quote
--- components/QuoteContainer.js
+++ components/QuoteContainer.js
@@ -1,46 +1,26 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import { AppLoading } from 'expo'
 import { Gyroscope, Accelerometer } from 'expo-sensors';
 import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet } from 'react-native'
+import styled from 'styled-components/native'
 
-let red = 0;
-let green = 0;
-let blue = 0;
-
-let hue = 1
+let hue = 0
 let saturation = 100
 let luminosity = 50
 
-const QuoteContainer = ({ text, textStyle }) => {
+const Quote = styled.Text`
+  textAlign: center;
+  fontFamily: Introspect-Bk;
+  color: ${props => props.hslColor};
+  width: 80%;
+  fontSize: ${props => props.quoteLength > 120 ? "35px" : "45px"};
+`
+
+const QuoteContainer = ({ quote }) => {
   const [gyroData, setGyroData] = useState({});
   const [accelData, setAccelData] = useState({});
-  Gyroscope.setUpdateInterval(100);
-  Accelerometer.setUpdateInterval(100);
-
-
-  const styles = StyleSheet.create({
-    container: {
-      width: "100%",
-      height: "100%",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    containerBgColor: {
-      width: 100,
-      height: 100,
-      // backgroundColor: `hsl(${hue}, ${saturation}%, ${luminosity}%)`,
-    },
-    text: {
-      textAlign: 'center',
-      fontFamily: 'Introspect-Bk',
-      // color: `${HSLToRGB(hue, saturation, luminosity)}`,
-      color: `white`,
-      width: "80%",
-      fontSize: 40,
-    },
-  });
+  Gyroscope.setUpdateInterval(40);
+  Accelerometer.setUpdateInterval(40);
 
   useEffect(() => {
     Gyroscope.addListener(gyroscopeData => {
@@ -54,31 +34,38 @@ const QuoteContainer = ({ text, textStyle }) => {
     });
   }, []);
 
-
   let { x: gyroX, y: gyroY, z: gyroZ } = gyroData;
   let { x: accelX, y: accelY, z: accelZ } = accelData;
 
-  hue = Math.abs(Math.floor(accelY * 360))
-  saturation = Math.floor(100 - Math.abs(accelX * 100))
-  luminosity = Math.floor(50 + (Math.max(gyroX, gyroY, gyroZ) * 5))
+  let luminosityRaw = 50 + (Math.max(gyroX, gyroY, gyroZ) * 10)
+  let saturationRaw = 100 - Math.abs(accelX * 100)
+  let hueRaw = accelY * 360
 
-  // red = Math.abs(Math.floor(round(x) * 255))
-  // green = Math.abs(Math.floor(round(y) * 255))
-  // blue = Math.abs(Math.floor(round(z) * 255))
+  luminosity = smooth(luminosity, luminosityRaw, 2)
+  saturation = smooth(saturation, saturationRaw, 5)
+  hue = smooth(hue, hueRaw, 5)
 
-  //hsl to rgb function
-  //https://css-tricks.com/converting-color-spaces-in-javascript/
-
+  const styles = StyleSheet.create({
+    background: {
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
 
   return (
     <>
-      <LinearGradient style={styles.container} colors={[`${HSLToRGB(hue, saturation, luminosity)}`, `${HSLToRGB(hue + 90, saturation, luminosity)}`]}>
-        <Text style={styles.text}>
-          {/* {text} */}
-          {/* x: {round(x)} y: {round(y)} z: {round(z)} */}
-          hue: {hue} sat: {saturation} lum: {luminosity}
-          RGB: {HSLToRGB(hue, saturation, luminosity)}
-        </Text>
+      <LinearGradient
+        style={styles.background}
+        start={[1, 0]}
+        colors={[`hsl(${hue}, ${saturation}%, ${luminosity}%)}`, `hsl(${hue + 67}, ${saturation}%, ${luminosity}%)}`]}>
+        <Quote
+          quoteLength={quote.length}
+          hslColor={`hsl(${hue - 45}, ${saturation + 10}%, ${luminosity - 5}%)`}>
+          {quote}
+        </Quote>
       </LinearGradient>
     </>
   );
@@ -86,46 +73,11 @@ const QuoteContainer = ({ text, textStyle }) => {
 
 export default QuoteContainer
 
-
-function round(n) {
-  if (!n) {
+function smooth(prevnumber, number, delay) {
+  if (!number) {
     return 0;
   }
-
-  return Math.floor(n * 10) / 10;
+  return Math.floor((prevnumber * (delay - 1) + number) / delay)
 }
 
 
-function HSLToRGB(h, s, l) {
-  // Must be fractions of 1
-
-  s /= 100;
-  l /= 100;
-
-  let c = (1 - Math.abs(2 * l - 1)) * s,
-    x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-    m = l - c / 2,
-    r = 0,
-    g = 0,
-    b = 0;
-
-
-  if (0 <= h && h < 60) {
-    r = c; g = x; b = 0;
-  } else if (60 <= h && h < 120) {
-    r = x; g = c; b = 0;
-  } else if (120 <= h && h < 180) {
-    r = 0; g = c; b = x;
-  } else if (180 <= h && h < 240) {
-    r = 0; g = x; b = c;
-  } else if (240 <= h && h < 300) {
-    r = x; g = 0; b = c;
-  } else if (300 <= h && h < 360) {
-    r = c; g = 0; b = x;
-  }
-  r = Math.round((r + m) * 255);
-  g = Math.round((g + m) * 255);
-  b = Math.round((b + m) * 255);
-
-  return "rgb(" + r + "," + g + "," + b + ")";
-}
