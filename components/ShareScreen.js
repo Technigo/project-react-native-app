a1c7ae658af34ec48ae31f2ccef60ac4dd0c53fe
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-08 17:56:46 +0100

    First attempt at basic functionality in place

--- components/ShareScreen.js
+++ components/ShareScreen.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { View, Share } from 'react-native';
+import React, { useState } from 'react';
+import { View, Share, Text, TextInput, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
 export const ShareScreen = () => {
+  const [message, setMessage] = useState('');
+
   const onShare = async () => {
     try {
       const result = await Share.share({
-        message:
-          'Place sweet TEST message I want to share here... <3',
+        message: `${message}`,
       });
       if (result.action === Share.sharedAction) {
         if (result.activityType) {
@@ -23,20 +24,35 @@ export const ShareScreen = () => {
     }
   };
   return (
-    <View style={{ marginTop: 50 }}>
+    <View style={{ marginTop: 50}}>
+      <Text>Enter your message here:</Text>
+      <TextInput style={styles.inputField} placeholder="Enter some text here" multiline={true} onChangeText={text => setMessage(text)} value={message}/>
       <ShareButton onPress={onShare}>
         <ButtonText>SHARE BUTTON TEST</ButtonText>
       </ShareButton>
     </View>
   );
-}
+};
 
+/*STYLED COMPONENTS AND STYLESHEETS*/
 const ShareButton = styled.TouchableOpacity`
   background: lightblue;
   width: 90px;
   padding: 10px;
+  margin: auto;
+  margin-top: 50px;
 `;
 
 const ButtonText = styled.Text`
   color: white;
 `;
+
+const styles = StyleSheet.create({
+  inputField: {
+    height: 150,
+    width: "70%",
+    borderColor: "tomato",
+    borderWidth: 2,
+    margin: "auto"
+  }
+});
