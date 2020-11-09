00f3bd59523f0d8c5ffc190468fef83c502c674c
Sofia Sousa <sofiavazsousa@gmail.com>
2020-11-09 16:40:45 +0100

    Add start screen

---
+++ Components/StartScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styled from 'styled-components';
+
+
+const OracleImage = styled.Image`
+  position: relative;
+  margin-left: 50px;
+  width: 250px;
+  height: 250px;
+`
+const StartText = styled.Text `
+  flex:0.5;
+  font-size: 30px;
+  color: #f2f0f0;
+`
+
+export const StartScreen = () => { 
+  return (
+    <View> 
+      <StartText> Shake me to get your Magic Oracle!</StartText>
+      <OracleImage
+        source={require('../assets/magic-ball.png')}
+      />
+
+    </View>
+  )
+};
