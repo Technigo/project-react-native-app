2b3168654ce747e4883f558d092f82883ba22020
Annika Melin <melin.anki>
2020-11-10 21:37:20 +0100

    v1 done: api fetch, styling and generating jokes

--- App.js
+++ App.js
@@ -1,25 +1,83 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import styled from 'styled-components/native'
+import { Button } from 'react-native'
 
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
   justify-content: center;
   align-items: center;
 `
 
+const Header = styled.View`
+  flex: 1;
+  width: 100%;
+  background-color: #DC4730;
+  justify-content: center;
+  align-items: center;
+  padding-top: 20px;
+`
+
 const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
+  color: white;
+  font-size: 20px;
+`
+
+const Main = styled.View`
+  flex: 7;
+  width: 100%;
+  background-color: #E7DFCD;
+  justify-content: center;
+  align-items: center;
+`
+
+const Joke = styled.Text`
+  font-size: 20px;
+  color: black;
+  padding: 20px;
+`
+
+const Footer = styled.View`
+  flex: 1;
+  width: 100%;
+  background-color: white;
+  justify-content: center;
+  align-items: center;
 `
 
 const App = () => {
+
+  const [joke, setJoke] = useState("")
+
+  const getJoke = () => {
+    fetch('https://icanhazdadjoke.com/', {
+      method: 'GET',
+      headers: {
+        "Accept": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(json => setJoke(json.joke))
+  }
+
+  useEffect(getJoke, [])
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
+
+      <Header>
+        <Title>The world's best dad jokes</Title>
+      </Header>
+
+      <Main>
+        <Joke>{joke}</Joke>
+      </Main>
+
+      <Footer>
+        <Button title="Generate new joke" onPress={getJoke} color="#032A34"></Button>
+      </Footer>
+
+    </Container >
   )
 }
 
