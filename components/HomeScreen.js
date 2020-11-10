1f1494c287a234bb94eab8d7514a91fbf5fe9589
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-10 12:36:56 +0100

    Start styling for HomeScreen, added title and title image

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
 
 export const HomeScreen= () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MainScreenContainer>
+        <TitleImageContainer>
+          <TitleImage source={require('../assets/home-title-img.png')} />
+        </TitleImageContainer>
+
+        <TitleText>MOOD BOOSTER</TitleText>
+      </MainScreenContainer>
+    </SafeAreaView>
   );
 };
+
+/*STYLED COMPONENTS*/
+const MainScreenContainer = styled.View`
+  padding-top: 20px;
+  background-color: white;
+`;
+
+const TitleImageContainer = styled.View`
+  margin: auto;
+  width: 90%;
+  height: 100px;
+`;
+
+const TitleImage = styled.Image`
+  height: 100%;
+  width: 240px;
+  margin: auto;
+`;
+
+const TitleText = styled.Text`
+  font-size: 20px;
+`;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  }
+});
