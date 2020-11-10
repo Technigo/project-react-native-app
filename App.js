d857b1f0ca4f258365b9a93e7f39e7629ad76284
caroline birgersson <caroline@birgerochblom.se>
2020-11-10 22:22:21 +0100

    started the project

--- App.js
+++ App.js
@@ -1,26 +1,48 @@
-import React from 'react'
-import styled from 'styled-components/native'
+import React from "react";
+import styled from "styled-components/native";
+import { Text, View, StyleSheet } from "react-native";
 
 const Container = styled.View`
   flex: 1;
   background-color: papayawhip;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Title = styled.Text`
   font-size: 24px;
-  color: palevioletred;
-`
+  color: blue;
+`;
 
 const App = () => {
   return (
-    <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
-  )
-}
+    <View style={styles.container}>
+      <Title style={styles.title}>MOVES</Title>
+      <Text style={styles.text}> {new Date().toLocaleDateString("en-En", {
+        weekday: "long"
+        })}
+      </Text>
+    </View>
+  );
+};
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFE3C4',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 60,
+    color: '#0025ff',
+    fontWeight: 'bold'
+  },
+  text: {
+    fontSize: 28,
+    color: '#0025ff',
+    textTransform: 'uppercase'
+  },
+});
+
+export default App;
