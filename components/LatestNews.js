994beeb800fb36e40e1792914f99550ecb9f0f8a
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-11-10 18:33:35 +0100

    Moment added displaying correct data

--- components/LatestNews.js
+++ components/LatestNews.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState }from 'react'
-import {FlatList, ScrollView, Text, View } from 'react-native'
+import {Text, View, Image, StyleSheet } from 'react-native'
+import moment from 'moment'
 
 export const LatestNews = () => {
   const API_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=88804fd27daf4133b20f9cd7037177bf'
@@ -11,13 +12,27 @@ export const LatestNews = () => {
     .then((json) => setLatestNews(json.articles))
     },[])
 
+    const styles = StyleSheet.create({
+      container: {
+        paddingTop: 50,
+      },
+      tinyLogo: {
+        width: 50,
+        height: 50,
+      },
+      logo: {
+        width: 66,
+        height: 58,
+      },
+    });
+
     return (
       <View>
         {latestNews.map((news) => (
           <>
-          <Text>{news.title}</Text>
+          <Text key={news.source.id}>{news.title}</Text>
           <Text>{news.author}</Text>
-          <Text>{news.publishedAt}</Text>
+          <Text>{moment(news.publishedAt).startOf('hour').fromNow()}</Text>
           {/* <Text>{news.source.name}</Text> */}
           </>
         ))}
