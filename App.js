9fd505db93b756453c64d50efa36e2db2acab472
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-11-10 23:34:56 +0100

    Images Added

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import {SafeAreaView, ScrollView, Image, Text, View } from 'react-native'
 
 import { LatestNews } from './components/LatestNews'
 
+import logo from './assets/live.png'
+
 
 const App = () => {
   return (
     <SafeAreaView>
-      {/* <BannerImage source={Banner} resizeMode={'cover'}/> */}
-      <Banner>BREAKING NEWS</Banner>
+      <BannerImage source={logo}/>
+      <Banner>// BREAKING NEWS //</Banner>
       <ScrollView>
         <LatestNews/>
       </ScrollView>
@@ -19,14 +21,14 @@ const App = () => {
 
 export default App
 
-
 const Banner = styled.Text`
-  font-size: 24px
+  font-size: 30px
+  fontWeight: 900
   color: palevioletred
   textAlign: center
+  marginBottom: 20px
 `
 
 const BannerImage = styled.Image`
-  height: 100px
-  width: 100%
+  paddingLeft: 20px
 `
--- components/LatestNews.js
+++ components/LatestNews.js
@@ -1,7 +1,10 @@
 import React, {useEffect, useState }from 'react'
+import {View, Image} from 'react-native'
 import styled from 'styled-components/native'
-import {Text, View, Image, StyleSheet } from 'react-native'
 import moment from 'moment'
+
+import refresh from '../assets/refresh.png'
+import clock from '../assets/clock.png'
 
 export const LatestNews = () => {
   const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=88804fd27daf4133b20f9cd7037177bf'
@@ -13,32 +16,23 @@ export const LatestNews = () => {
     .then((json) => setLatestNews(json.articles))
     },[])
 
-    const styles = StyleSheet.create({
-      container: {
-        paddingTop: 50,
-      },
-      tinyLogo: {
-        width: 50,
-        height: 50,
-      },
-      logo: {
-        width: 66,
-        height: 58,
-      },
-    });
-
     return (
       <View>
         {latestNews.map((news) => (
           <Container>
-          <TitleText key={news.source.id}>{news.title}</TitleText>
-          <AuthorName>{news.author}</AuthorName>
-          <TimedText>{moment(news.publishedAt).startOf('hour').fromNow()}</TimedText>
-          {/* <Text>{news.source.name}</Text> */}
+            <TitleText key={news.source.id}>
+              {/* <ClockImage source={clock}/> */}
+              {news.title}
+            </TitleText>
+            <AuthorName>{news.author}</AuthorName>
+            <TimedText>
+              {/* <RefreshImage source={refresh}/> */}
+              {moment(news.publishedAt).startOf('hour').fromNow()}
+            </TimedText>
+            {/* <Text>{news.source.name}</Text> */}
           </Container>
         ))}
       </View>
-    
     )
 }
 const Container = styled.View`
@@ -50,7 +44,7 @@ const Container = styled.View`
 `
 
 const TitleText = styled.Text `
-  fontSize: 20px
+  fontSize: 30px
 `
 
 const TimedText = styled.Text `
@@ -62,5 +56,12 @@ const TimedText = styled.Text `
 
 const AuthorName = styled.Text `
   marginTop: 10px
+`
+
+const ClockImage = styled.Image `
+
+`
+
+const RefreshImage = styled.Image `
 
 `
