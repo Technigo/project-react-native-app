3af979669f612804156faa35cdace696424a5487
mats-johansen <johansenmats89@gmail.com>
2020-11-10 09:49:42 +0100

    testing

--- App.js
+++ App.js
@@ -1,24 +1,72 @@
-import React from 'react'
-import styled from 'styled-components/native'
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+import { TouchableOpacity, Text } from 'react-native';
+
+
+
+
+const MainText = styled.Text`
+  font-size: 24px;
+`;
+
+const OrangeMainText = styled(MainText)`
+  color: #ff2255;
+`;
 
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
   justify-content: center;
   align-items: center;
-`
+`;
 
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
+const TopContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  background-color: #222222;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TopHeader = styled.Text`
+  font-size: 48px;
+  color: #efefef;
+`;
+
+const BottomContainer = styled.View`
+  flex: 3;
+  width: 100%;
+  background-color: #ffffff;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BottomText = styled.Text`
+  font-size: 32px;
+  color: #1f1f1f;
+`;
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const onIncrement = () => {
+    setCount(count + 1);
+    console.log('incremented');
+  }
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
+      <TopContainer>
+        <TopHeader>
+          Counter
+        </TopHeader>
+      </TopContainer>
+      <BottomContainer>
+        <TouchableOpacity onPress={onIncrement}>
+          <Text>Add +1</Text>
+        </TouchableOpacity>
+        <BottomText>
+          Total: {count}
+        </BottomText>
+      </BottomContainer>
     </Container>
   )
 }
