fc594d0398d0947839efbd4d22725d3073c32557
Katsiez <katarinajovanovic.a@gmail.com>
2020-11-10 21:29:01 +0100

    just wanted to see if this would work

--- App.js
+++ App.js
@@ -1,48 +1,21 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
 import styled from 'styled-components/native'
-
-const Img = styled.Image`
-  width: 450px;
-  height: 1000px;
-`;
-
-const Container = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
-  align-items: center;
-`
-
-const Title = styled.Text`
-  font-size: 24px;
-  backgroundColor: #f8f7f7;
-  position:absolute;
-  padding:20px;
-  text-align:center;
-  color:#352727;
-`
-const TextText = styled.Text`
-  font-size: 24px;
-  position:absolute;
-  color:#352727;
-  bottom:300px;
-  text-align:center;
-
-`
-
 
 export default function App() {
   return (
-    <Container>
-      <View>
-        <Container>
-          <Img source={require('./assets/background.jpg')} />
-          <Title>Cozy Fall reads</Title>
-          <TextText>Find out some of the coziest fall reads</TextText>
-        </Container>
-      </View>
-    </Container>
+    <View style={styles.container}>
+      <Text>Some kinda text here</Text>
+      <StatusBar style="auto" />
+    </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0000',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
---
+++ src/screens/Detail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class Detail extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Details</Text>
+            </View>
+        )
+    }
+}
