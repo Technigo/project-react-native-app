ca8760bd07fc6bdf936b55702233b2ac97c02d7a
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-11-10 22:01:46 +0100

    Some Styling Added

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
 import React from 'react'
 import styled from 'styled-components/native'
+import {SafeAreaView, ScrollView, Image, Text, View } from 'react-native'
 
-import {SafeAreaView, ScrollView, Text, View } from 'react-native'
 import { LatestNews } from './components/LatestNews'
 
-const Container = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
-  align-items: center;
-`
-
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
+      {/* <BannerImage source={Banner} resizeMode={'cover'}/> */}
+      <Banner>BREAKING NEWS</Banner>
       <ScrollView>
-      <LatestNews/>
+        <LatestNews/>
       </ScrollView>
     </SafeAreaView>
   )
 }
 
 export default App
+
+
+const Banner = styled.Text`
+  font-size: 24px
+  color: palevioletred
+  textAlign: center
+`
+
+const BannerImage = styled.Image`
+  height: 100px
+  width: 100%
+`
--- components/LatestNews.js
+++ components/LatestNews.js
@@ -1,13 +1,14 @@
 import React, {useEffect, useState }from 'react'
+import styled from 'styled-components/native'
 import {Text, View, Image, StyleSheet } from 'react-native'
 import moment from 'moment'
 
 export const LatestNews = () => {
-  const API_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=88804fd27daf4133b20f9cd7037177bf'
+  const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=88804fd27daf4133b20f9cd7037177bf'
   const [latestNews, setLatestNews] = useState([])
 
   useEffect (() => { 
-  fetch (API_URL)
+  fetch (apiUrl)
     .then((response) => response.json())
     .then((json) => setLatestNews(json.articles))
     },[])
@@ -29,14 +30,37 @@ export const LatestNews = () => {
     return (
       <View>
         {latestNews.map((news) => (
-          <>
-          <Text key={news.source.id}>{news.title}</Text>
-          <Text>{news.author}</Text>
-          <Text>{moment(news.publishedAt).startOf('hour').fromNow()}</Text>
+          <Container>
+          <TitleText key={news.source.id}>{news.title}</TitleText>
+          <AuthorName>{news.author}</AuthorName>
+          <TimedText>{moment(news.publishedAt).startOf('hour').fromNow()}</TimedText>
           {/* <Text>{news.source.name}</Text> */}
-          </>
+          </Container>
         ))}
       </View>
     
     )
 }
+const Container = styled.View`
+  margin: 10px
+  flex: 1
+  border: 2px solid grey
+  padding: 20px
+  borderRadius: 30px
+`
+
+const TitleText = styled.Text `
+  fontSize: 20px
+`
+
+const TimedText = styled.Text `
+  flex:1
+  marginTop: 10px
+  fontSize: 15px
+  textAlign: right
+`
+
+const AuthorName = styled.Text `
+  marginTop: 10px
+
+`
