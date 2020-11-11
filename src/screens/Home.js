2c3b3d2b6c5aec775886250ba4a29b83ceb437db
Katsiez <katarinajovanovic.a@gmail.com>
2020-11-11 11:33:24 +0100

    setting up some fictional data numbers until i find a good api

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,6 +9,28 @@ import {
 } from 'react-native'
 import Icon from '@expo/vector-icons/Ionicons'
 import { ScrollView } from 'react-native-gesture-handler'
+import Deck from '../components/Deck'
+import Cards from '../components/Cards'
+import Buttons from '../components/Buttons'
+
+const DATA = [
+
+    {
+        id: 1,
+        title: "NUMBER OF CASES",
+        number: "1 838 456"
+    },
+    {
+        id: 1,
+        title: "TOTAL DEATHS",
+        number: "1 029 863"
+    },
+    {
+        id: 1,
+        title: "RECOVERED",
+        number: "838 456"
+    }
+]
 export default class Home extends Component {
     render() {
         return (
