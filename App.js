7eb7a3ea131c5eab31e061fb8b7e39a4d0d004ca
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-11-11 13:51:19 +0100

    added flatlist and header

--- App.js
+++ App.js
@@ -1,26 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/native'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
 
-const Container = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
-  align-items: center;
-`
-
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
+import { Header } from './components/Header'
 
 const App = () => {
+  const [toDos, setTodos] = useState ([
+    {task: 'Work on my project', key: 1},
+    {task: 'Workout', key: 2},
+    {task: 'Have a coffee', key: 3},
+    {task: 'Go for a walk', key: 4}
+  ])
+  
   return (
-    <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
+    <Wrapper>
+       <Header />
+      <TodoContainer>
+       <TodoList>
+        <FlatList
+           data={toDos}
+           renderItem={({ item }) =>(
+             <TodoText>{item.task}</TodoText>
+           )}
+        />
+       </TodoList>
+      </TodoContainer>
+     
+      </Wrapper>
+
   )
-}
+}   
+
+const Wrapper = styled.View`
+flex: 1
+align-items: center
+`
+const TodoContainer = styled.View`
+padding 40px
+`
+const TodoList = styled.View`
+margin-top: 30px
+`
+const TodoText = styled.Text`
+font-size: 20px
+font-weight: bold
+`
+
+
+
+
+
+// const Touchable = styled.TouchableOpacity`
+// `
+// const TextField = styled.Text`
+// border-width: 1
+// color: #777
+// padding: 8px
+// margin: 10px
+// width: 200px
+// `
+// const UppdateButton = styled.TouchableOpacity`
+// margin-top: 10px
+// background: blue
+// padding: 5px
+// border-radius: 5px
+// `
+// const ButtonText = styled.Text`
+// font-weight: bold
+// `
+
+
 
 export default App
---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import { View, Text } from 'react-native'
+
+export const Header = () => {
+
+
+    return (
+        <HeaderContainer>
+            <HeaderText>TODOS</HeaderText>
+        </HeaderContainer>
+    )
+}
+const HeaderContainer = styled.View`
+  height: 80px
+  background-color: #48D1CC
+  padding: 10px
+  width: 100%
+  align-items: center
+  justify-content: center
+  `
+const HeaderText = styled.Text`
+font-weight: bold
+font-size: 20px
+`
+
