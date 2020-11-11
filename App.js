7f7395a642af247075370f9e4733446d95175052
Annika Melin <melin.anki>
2020-11-11 22:28:33 +0100

    added joke search, removed dad and hipster jokes, finalized styling

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import styled from 'styled-components/native'
-import { Button } from 'react-native'
+import { Button, Share } from 'react-native'
 
 const Container = styled.View`
   flex: 1;
@@ -11,46 +11,67 @@ const Container = styled.View`
 
 const Header = styled.View`
   flex: 1;
-  width: 100%;
-  background-color: #DC4730;
   justify-content: center;
   align-items: center;
+
   padding-top: 20px;
+  width: 100%;
+  background-color: #DC4730;
 `
 
 const Title = styled.Text`
-  color: white;
   font-size: 24px;
   font-weight: 900;
   font-style: italic;
+  color: white;
 `
 
-const Main = styled.View`
-  flex: 5;
+const JokeContainer = styled.View`
+  flex: 6;
+  justify-content: center;
   width: 100%;
   background-color: #E7DFCD;
-  justify-content: center;
-  align-items: center;
 `
 
 const Joke = styled.Text`
+  padding: 0 40px;
   font-size: 20px;
   color: #032A34;
-  padding: 40px;
 `
 
-const Footer = styled.View`
-  flex: 3;
-  width: 100%;
-  background-color: white;
-  justify-content: space-evenly;
+const ButtonContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
+  padding: 20px 40px;
+  width: 100%;
+  background-color: #E7DFCD;
+`
+
+const SearchContainer = styled.View`
+  flex: 1;  
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 40px;
+  width: 100%;
+  background-color: #E7DFCD;  
+`
+
+const Input = styled.TextInput`
+  padding: 5px;
+  height: 40px;
+  width: 70%;
+  border-width: 1px;
+  border-radius: 8px;
+  border-color: #032A34; 
 `
 
 const App = () => {
 
   const [joke, setJoke] = useState("")
-  const [jokes, setJokes] = useState([])
+  const [search, setSearch] = useState()
 
   const getJoke = () => {
     fetch('https://icanhazdadjoke.com/', {
@@ -63,8 +84,8 @@ const App = () => {
       .then(json => setJoke(json.joke))
   }
 
-  const getDadJoke = () => {
-    fetch('https://icanhazdadjoke.com/search?term=dad', {
+  const searchJoke = () => {
+    fetch(`https://icanhazdadjoke.com/search?term=${search}`, {
       method: 'GET',
       headers: {
         "Accept": "application/json"
@@ -74,21 +95,27 @@ const App = () => {
       .then(json => {
         const randomIndex = Math.floor(Math.random() * Math.floor(json.results.length))
         setJoke(json.results[randomIndex].joke)
+        setSearch("")
+        setSearch()
+      })
+      .catch(() => {
+        alert("No such joke")
+        setSearch("")
+        setSearch()
       })
   }
 
-  const getHipsterJokes = () => {
-    fetch('https://icanhazdadjoke.com/search?term=hipster', {
-      method: 'GET',
-      headers: {
-        "Accept": "application/json"
-      }
-    })
-      .then(response => response.json())
-      .then(json => {
-        setJoke(null)
-        setJokes(json.results)
+  const shareJoke = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          `${joke}`,
       })
+      result.action === Share.sharedAction ? (alert("Joke shared")) :
+        result.action === Share.dismissedAction ? (alert("Joke not shared")) : null
+    } catch (error) {
+      alert(error.message)
+    }
   }
 
   useEffect(getJoke, [])
@@ -100,22 +127,22 @@ const App = () => {
         <Title>The world's best dad jokes</Title>
       </Header>
 
-      <Main>
-        {joke ?
-          <Joke>{joke}</Joke> :
-          jokes ?
-            jokes.map(item => {
-              <Joke>{item.joke}</Joke>
-              console.log(item.joke)
-            }) : null
-        }
-      </Main>
+      <SearchContainer>
+        <Input
+          onChangeText={text => setSearch(text)}
+          value={search}
+        />
+        <Button title="Search" onPress={searchJoke} color="#032A34"></Button>
+      </SearchContainer>
 
-      <Footer>
-        <Button title="Generate new joke" onPress={getJoke} color="#032A34"></Button>
-        <Button title="Jokes including 'dad'" onPress={getDadJoke} color="#032A34"></Button>
-        <Button title="All hipster jokes" onPress={getHipsterJokes} color="#032A34"></Button>
-      </Footer>
+      <JokeContainer>
+        <Joke>{joke}</Joke>
+      </JokeContainer>
+
+      <ButtonContainer>
+        <Button title="Generate" onPress={getJoke} color="#032A34"></Button>
+        <Button title="Share" onPress={shareJoke} color="#032A34"></Button>
+      </ButtonContainer>
 
     </Container >
   )
