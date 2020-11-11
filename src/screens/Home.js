cdab657ea5d6a6aa92afb4cb84b28cf13e474de7
Katsiez <katarinajovanovic.a@gmail.com>
2020-11-11 14:51:41 +0100

    Got part of the home image to display on web

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,6 +12,7 @@ import { ScrollView } from 'react-native-gesture-handler'
 import Deck from '../components/Deck'
 import Cards from '../components/Cards'
 import Buttons from '../components/Buttons'
+import styles from 'styled-components'
 
 const DATA = [
 
@@ -32,9 +33,52 @@ const DATA = [
     }
 ]
 export default class Home extends Component {
+
+    renderCard(item) {
+        return (
+            <View key={item.id} style={styles.cardContainer}>
+                <View styles={styles.card}>
+                    <View>
+                        <Text style={styles.title}>{item.title}</Text>
+                        <Icon
+                            name="ios-remove"
+                            size={40}
+                            color="blue"
+                            style={{ marginTop: 25 }}
+                        />
+                        <Text style={styles.title}>{item.title}</Text>
+                    </View>
+                    <View style={{ marginLeft: 150 }}>
+                        <Icon name="md-options" size={24} color='#FFF' />
+                        <Text style={styles.textCovid}>COVID-19</Text>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+
+    renderNoMoreCards() {
+        return (
+            <View title="All done">
+                <Text style={styles.noCard}>no more cards here</Text>
+                <Button backgroundColor="03A9F4" title="Get more!"></Button>
+            </View>
+        )
+    }
     render() {
         return (
-            <View>
+            <View style={styles.container}>
+                <ImageBackground
+                    source={require("../images/background.jpg")}
+                    style={styles.map}>
+
+                    <View style={styles.col}>
+                        <View style={{ width: "50%" }}>
+                            <Icon name="md-remove" color="#FFF" size={26} />
+                            <Icon name="md-remove" color="#FFF" size={26} style={styles.minusIcon} />
+                        </View>
+                    </View>
+                </ImageBackground>
                 <Text>Home</Text>
             </View>
         )
