670d09d4e0e95298bf5cccc6c2060ca24ec7f68d
ELLEN BJORKMAN <ELLEN881008@HOTMAIL.SE>
2020-11-11 11:23:17 +0100

    install expo-sensors. Get basic data from pedometer

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native'
+import { Pedometer } from 'expo-sensors'
 
 const Container = styled.View`
   flex: 1;
@@ -11,16 +12,51 @@ const Container = styled.View`
 const Title = styled.Text`
   font-size: 24px;
   color: palevioletred;
+  background: black;
+  margin: 10px;
+  color: white;
+  padding: 20px;
 `
-
+/* getStepCountAsync - Returns a promise that resolves to an Object with a steps key, which is a Number indicating the number of steps taken between the given dates. */
 const App = () => {
+  const [steps, setSteps] = useState(0)
+  const [weeklySteps, setWeeklySteps] = useState(0)
+  const [todaysSteps, setTodaysSteps] = useState(0)
+  const stepsLeft = 10000-todaysSteps
+
+  const endWeek = new Date()
+  const startWeek = new Date()
+  startWeek.setDate(endWeek.getDate() - 7)
+
+  const endToday = new Date()
+  const startToday = new Date()
+  startToday.setDate(endToday.getDate() - 1)
+
+  useEffect(()=>{
+    Pedometer.watchStepCount(result => {
+      setSteps(result.steps)
+    })
+    Pedometer.getStepCountAsync(startWeek, endWeek)
+      .then(result => {
+        setWeeklySteps(result.steps)
+    })
+    Pedometer.getStepCountAsync(startToday, endToday)
+      .then(result => {
+        setTodaysSteps(result.steps)
+    })
+  }, [steps])
+  /* should I have more dependencies here or is this enough? every time steps updates the useEffect updates the states */
+
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
+      <Title>Steps this week: {weeklySteps}</Title>
+      <Title>Steps last 24 hours: {todaysSteps}</Title>
+      <Title>Current steps: {steps}</Title>
+      <Title>{stepsLeft}</Title>
     </Container>
   )
 }
 
 export default App
+
