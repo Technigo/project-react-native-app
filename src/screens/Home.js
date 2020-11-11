c1ed9f82b50ce4417049d9d39861429e3fce2ae3
Katsiez <katarinajovanovic.a@gmail.com>
2020-11-11 17:38:30 +0100

    still not showing up on the phone...

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
     View,
     Text,
@@ -6,28 +6,28 @@ import {
     Image,
     Button,
     ImageBackground
-} from 'react-native'
-import Icon from '@expo/vector-icons/Ionicons'
-import { ScrollView } from 'react-native-gesture-handler'
-import Deck from '../components/Deck'
-import Cards from '../components/Cards'
-import Buttons from '../components/Buttons'
-import styles from 'styled-components'
+} from 'react-native';
+import Icon from '@expo/vector-icons/Ionicons';
+import { ScrollView }
+    from 'react-native-gesture-handler'
+import Deck from '../components/Deck';
+import Cards from '../components/Cards';
+import Buttons from '../components/Buttons';
+
 
 const DATA = [
-
     {
         id: 1,
-        title: "NUMBER OF CASES",
+        title: "CORONAVIRUS CASES",
         number: "1 838 456"
     },
     {
-        id: 1,
+        id: 2,
         title: "TOTAL DEATHS",
-        number: "1 029 863"
+        number: "1 29 863"
     },
     {
-        id: 1,
+        id: 3,
         title: "RECOVERED",
         number: "838 456"
     }
@@ -37,19 +37,19 @@ export default class Home extends Component {
     renderCard(item) {
         return (
             <View key={item.id} style={styles.cardContainer}>
-                <View styles={styles.card}>
+                <View style={styles.card}>
                     <View>
                         <Text style={styles.title}>{item.title}</Text>
                         <Icon
                             name="ios-remove"
                             size={40}
-                            color="blue"
+                            color="red"
                             style={{ marginTop: 25 }}
                         />
-                        <Text style={styles.title}>{item.title}</Text>
+                        <Text style={styles.number}>{item.number}</Text>
                     </View>
                     <View style={{ marginLeft: 150 }}>
-                        <Icon name="md-options" size={24} color='#FFF' />
+                        <Icon name="md-options" size={24} color="#FFF" />
                         <Text style={styles.textCovid}>COVID-19</Text>
                     </View>
                 </View>
@@ -59,82 +59,196 @@ export default class Home extends Component {
 
     renderNoMoreCards() {
         return (
-            <View title="All done">
-                <Text style={styles.noCard}>no more cards here</Text>
-                <Button backgroundColor="03A9F4" title="Get more!"></Button>
+            <View title="All Domne!">
+                <Text style={styles.noCard}>NO MORE CARDS HERE</Text>
+                <Button backgroundColor="#03A9F4" title="Get more!" />
             </View>
-        )
+        );
     }
+
     render() {
         return (
             <View style={styles.container}>
                 <ImageBackground
                     source={require("../images/background.jpg")}
-                    style={styles.map}>
-                    {/* <ImageBackground
-          source={require('../images/background.jpg')}
-          style={{ width: '100%' }}
-        ></ImageBackground> */}
-
+                    style={styles.map}
+                >
                     <View style={styles.col}>
                         <View style={{ width: "50%" }}>
                             <Icon name="md-remove" color="#FFF" size={26} />
-                            <Icon name="md-remove" color="#FFF" size={26} style={styles.minusIcon} />
+                            <Icon
+                                name="md-remove"
+                                color="#FFF"
+                                size={26}
+                                style={styles.minusIcon}
+                            />
                         </View>
                         <View style={styles.avatarContainer}>
                             <Image
                                 source={require('../images/bookshelf.png')}
-                                style={styles.avatar} />
+                                style={styles.avatar}
+                            />
                         </View>
                     </View>
-                    <Text style={styles.textDash}>Corona dashboard</Text>
+                    <Text style={styles.textDash}>CORONA DASH</Text>
 
                     <View style={styles.colContainer}>
                         <Text style={styles.textGlobal}>GLOBAL</Text>
                         <Text style={styles.textSweden}>SWEDEN</Text>
                         <View style={styles.reloadContainer}>
-                            <Icon name="md-refresh" size={24} color="green" />
+                            <Icon name="md-refresh" size={24} color="red" />
                         </View>
                     </View>
                 </ImageBackground>
                 <Deck
                     data={DATA}
                     renderCard={this.renderCard}
-                    renderNoMoreCards={this.renderNoMoreCards} />
+                    renderNoMoreCards={this.renderNoMoreCards}
+                />
                 <ScrollView
                     style={{ marginTop: 170 }}
                     showsHorizontalScrollIndicator={false}
-                    horizontal>
-
+                    horizontal
+                >
                     <Cards
+                        onPress={() => this.props.navigation.navigate('Detail')}
                         icon="md-pulse"
                         title="TOTAL CASES"
                         bg="red"
-                        number="112 329" />
-
+                        number="113 329"
+                    />
                     <Cards
                         icon="ios-git-network"
                         title="RECOVERED"
-                        bg="blue"
-                        number="442 329" />
-
+                        bg="#FFF"
+                        number="442 329"
+                    />
                     <Cards
                         icon="ios-heart-dislike"
                         title="DEATH CASES"
-                        bg="red"
-                        number="113 329" />
+                        bg="#FFF"
+                        number="113 329"
+                    />
                 </ScrollView>
                 <View style={{ marginBottom: 34 }}>
                     <Buttons
-                        name="ASYMPTOPMATIC"
-                        number="1 778" />
+                        name="ASYMPTOMATIC"
+                        number="1 778"
+                    />
                     <Buttons
                         name="SYMPTOMATIC"
-                        number="1 578" />
-
+                        number="1 578"
+                    />
 
                 </View>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#1c2732"
+    },
+    cardContainer: {
+        height: 150,
+        width: 320,
+        alignSelf: "center",
+        backgroundColor: "#6A706E",
+        borderRadius: 30
+    },
+    card: {
+        height: 150,
+        width: 260,
+        paddingTop: 20,
+        paddingHorizontal: 30,
+        backgroundColor: '#2b3240',
+        borderRadius: 30,
+        flexDirection: 'row'
+    },
+    title: {
+        color: "#6A706E",
+        width: 100,
+        fontSize: 12,
+        fontWeight: "bold"
+    },
+    number: {
+        color: "#FFF",
+        width: 100,
+        fontSize: 22,
+        fontWeight: "bold",
+        marginTop: -10,
+    },
+    textCovid: {
+        transform: [{ rotate: "-90deg" }],
+        color: "#3a4b4f",
+        fontSize: 14,
+        width: 90,
+        marginLeft: -35,
+        fontWeight: 'bold',
+        marginTop: 20
+    },
+    noCard: {
+        marginBottom: 10,
+        color: '#FFF',
+        alignSelf: "center"
+    },
+    map: {
+        height: 200,
+        paddingTop: 25,
+        paddingHorizontal: 20,
+        marginBottom: 15
+    },
+    col: {
+        flexDirection: 'row'
+    },
+    minusIcon: {
+        marginTop: -20,
+        marginLeft: 5
+    },
+    avatarContainer: {
+        width: "50%",
+        alignItems: 'flex-end',
+    },
+    avatar: {
+        width: 40,
+        height: 40,
+        borderRadius: 20
+    },
+    textDash: {
+        color: "#FFF",
+        fontSize: 20,
+        alignSelf: 'center',
+        marginTop: 15,
+        fontWeight: 'bold'
+    },
+    colContainer: {
+        flexDirection: "row",
+        paddingHorizontal: 30,
+        marginTop: 40,
+        alignItems: 'center',
+    },
+    textGlobal: {
+        fontWeight: "bold",
+        fontSize: 16,
+        color: "red"
+    },
+    textSweden: {
+        fontWeight: "bold",
+        fontSize: 16,
+        paddingHorizontal: 30,
+        color: "#6a706e"
+    },
+    reloadContainer: {
+        backgroundColor: "#FFF",
+        elevation: 2,
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: "center",
+        marginLeft: 50
+    }
+
+
+});
