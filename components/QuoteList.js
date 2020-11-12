b24258994856e51fb64bc1ba060b1f4521f9b97a
ClaireCaudwell <clairecaudwell@gmail.com>
2020-11-12 16:56:02 +0100

    Updated QuoteList.js: 1. Imported SafeAreaView and StyleSheet from react native. 2. Imported styled components. 3. Imported StaticImages.js. 4. Added the main react native jsx. 5. In the ButtonContainer I map through the data passed from the Quotes.json so the link for the 6 different quotes is created. 6. Also created an image tag whereby I get the image for each quote/link from the StaticImage.js based on the id of the image. 7. Used resizeMode so the images will be contained within the area they're defined. 8. Added styled components and also a stylesheet for the SafeAreaView.

--- components/QuoteList.js
+++ components/QuoteList.js
@@ -1,20 +1,92 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import styled from 'styled-components/native'
 
 import Quotes from '../data/quotes.json';
+import StaticImages from './StaticImage.js';
 
-const QuoteList = ({navigation}) => {
+export const QuoteList = ({navigation}) => {
     return ( 
-        <View>
-            {Quotes.map((quotes) => (
-                <Button 
-                    key={quotes.id}                
-                    title="Detail"
-                    onPress={() => navigation.navigate('Detail', quotes)}
-                />
-            ))}
-        </View>      
+        <SafeAreaView style={styles.container}>
+            <ViewContainer>
+                <TextContainer>
+                    <TitleText>POSITIVE QUOTES</TitleText>
+                    <SubtitleText>Click on a symbol to read a quote</SubtitleText>
+                </TextContainer>
+                <ButtonContainer>                
+                    {Quotes.map((quotes) => (
+                        <IconButton
+                            title={'quotes.id'} 
+                            onPress={()=> navigation.navigate('Detail', quotes)} 
+                            key={quotes.id}>                    
+                            <IconImage source={StaticImages[quotes.id]} resizeMode={'contain'}/> 
+                        </IconButton>
+                    ))}               
+                </ButtonContainer>
+            </ViewContainer>
+        </SafeAreaView>      
     );
 };
 
-export default QuoteList;
+const ViewContainer = styled.View`
+    background-color: white;
+    align-items: center;
+    
+`;
+
+const TextContainer = styled.View`
+    flex-direction: row;
+`;
+
+const TitleText = styled.Text`
+    font-size: 20px;
+    color: #fff;
+    text-align: center;
+    background-color: rgb(236, 222, 95);
+    padding: 30px 20px;
+    font-weight: 600;
+`;
+
+const SubtitleText = styled.Text`
+    font-size: 18px;
+    text-align: center;
+    color: #fff;
+    font-weight: 400;
+    background-color: rgb(255, 192, 203);
+    padding: 30px 20px;
+`; 
+
+const ButtonContainer = styled.View`
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+`;
+
+const IconButton = styled.TouchableOpacity`
+    width: 50%; 
+    height: 100%;  
+    padding: 5px;    
+`;
+
+const IconImage = styled.Image`
+    width: 100%;
+    height: 100%;     
+`;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "rgb(255, 255, 255);",
+    }
+});
+
+/* 
+SafeAreaView: Renders content within the safe boundaries of a device
+View: A cotainer that supports layout with flexbox, style etc. 
+*/
+
+
+
