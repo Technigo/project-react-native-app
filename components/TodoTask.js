e26b1b111b17cfbfa2082242e734d87564b34d6d
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-11-12 13:50:40 +0100

    added delete-button

--- components/TodoTask.js
+++ components/TodoTask.js
@@ -1,26 +1,51 @@
 import React from 'react'
 import styled from 'styled-components/native'
-import { TouchableOpacity, Text } from 'react-native'
+import { TouchableOpacity, Text, View, Image } from 'react-native'
+
+import doneImage from "../assets/done-sign.png"
 
 export const TodoTask = ({ item, pressHandler }) => {
 
-    return(
-        <TouchableText onPress={() => pressHandler(item.key)}>
-           <TodoText>{item.task}</TodoText>
-        </TouchableText>
+    return (
+        <TaskContainer>
+            <TextContainer>
+                <TodoText>{item.task}</TodoText>
+            </TextContainer>
+            <TouchableText onPress={() => pressHandler(item.key)}>
+            <Checkmark>&#10004;</Checkmark>
+            </TouchableText>
+        </TaskContainer>
     )
 }
 
-const TouchableText = styled.TouchableOpacity`
+const TaskContainer= styled.View`
+flex-direction: row
+`
+const TextContainer= styled.View`
 margin-top: 20px;
 padding: 10px;
 background-color: #e6e6e6
 border-width: 1
 border-radius: 5px
 width: auto
+align-items: center
+`
+const TouchableText = styled.TouchableOpacity`
+margin-top: 20px;
+padding: 10px;
+background-color: #90EE90
+border-width: 1
+border-radius: 5px
+width: 50px
+align-items: center
+margin-left: 10px
 `
 const TodoText = styled.Text`
 font-size: 20px
 font-weight: bold
-
 `
+const Checkmark = styled.View`
+font-size: 20px
+`
+
+
