18ca551b0232f34477490c9d0d95eab1de67c639
Max Forssén <max.forssen@mentimeter.com>
2020-11-12 17:07:15 +0100

    input field, button, fetch hearthstone card based on input

--- App.js
+++ App.js
@@ -1,24 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components/native'
+import { TextInput } from 'react-native';
 
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 `
 
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
+const Card = styled.Image`
+  width: 400px;
+  height: 600px;
+`
+
+const Button = styled.Button`
+  
 `
 
 const App = () => {
+
+  const [fetched, setFetched] = useState(false)
+  const [value, onChangeText] = useState('Enter card name!');
+  const [data, getData] = useState([])
+
+  const fetchCard = () => {
+
+    fetch(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/${value}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "9e4bc04d24mshe167fe2dbc59904p1dda1ejsnb8edfa52d102",
+        "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com"
+      }
+    })
+    .then((response) => response.json())
+    .then(response => {
+      getData(response)
+      setFetched(true)
+      console.log(response)
+      console.log(response[0].img)
+    })
+    .catch(err => {
+      console.error(err);
+    })
+    
+    }
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+      />
+      <Button onPress={fetchCard} title="Learn More" /> 
+
+      { fetched === true ? (
+      <Card source={{uri:`${data[0].img}`,}} />  
+      )  : null}
+
     </Container>
   )
 }
