e7811efcf351f9f178597f88a71df03d8e528148
Therese <64402455+Thessan@users.noreply.github.com>
2020-11-12 13:01:45 +0100

    first upload

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
+import { FortuneMessage } from './Components/FortuneMessage'
+
+  export const App = () => {
+
+  return (
+    <>
+    <Container>
+      <FortuneMessage />
+    </Container>
+    </>
+  )
+}
+
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
+  background-color: pink;
   justify-content: center;
   align-items: center;
 `
-
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
-
-const App = () => {
-  return (
-    <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
-  )
-}
 
 export default App
