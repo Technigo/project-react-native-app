9deee618bbe0af2a4238514c65dcb13eb1062832
Katsiez <katarinajovanovic.a@gmail.com>
2020-11-13 14:57:49 +0100

    got a chart to display thanks to this github https://github.com/indiespirit/react-native-chart-kit but it isn't totally functional so working on that

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -24,12 +24,36 @@ export default class Chart extends Component {
                                     Math.random() * 100,
                                     Math.random() * 100
                                 ],
-                            }
-                        ]
-                    }}>
+                            },
+                        ],
+                    }}
+                    width={Dimensions.get('window').width}
+                    height={320}
+                    yAxisSuffix='k'
+                    yAxisInterval={1}
+                    chartConfig={{
+                        backgroundColor: '#fff',
+                        backgroundGradientFrom: '#fff',
+                        backgroundGradientTo: '#fff',
+                        decimalPlaces: 2,
+                        color: (opacity = 0) => `rgba(255,0,0, ${opacity})`,
+                        labelColor: (opacity = 0) => `rgba(0,0,0, ${opacity})`,
+                        style: {
+                            borderRadius: 16
+                        },
+                        propsForDots: {
+                            r: '6',
+                            strokeWidth: "2",
+                            stroke: "red"
+                        },
+                    }}
+                    bezier
+                    style={{
+                        marginVertical: 8,
+                        borderRadius: 16
+                    }}
 
-                </LineChart>
-
+                />
             </View>
         )
     }
