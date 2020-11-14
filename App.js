51bceffd875be2920dd61ac27c777a4f21252bb9
ClaireCaudwell <clairecaudwell@gmail.com>
2020-11-14 11:41:38 +0100

    Updated App.js: 1. Imported the components for each stack and created each stack that represents a quote page.

--- App.js
+++ App.js
@@ -3,8 +3,14 @@ import React  from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { QuoteList } from './components/QuoteList';
-import { QuoteDetail } from './components/QuoteDetail';
+import QuoteList from './components/QuoteList';
+import Heart from './components/Heart';
+import Sun from './components/Sun';
+import Flower from './components/Flower';
+import Bird from './components/Bird';
+import Hand from './components/Hand';
+import Leaf from './components/Leaf';
+import BlueFlower from './components/BlueFlower';
 
 const  Stack = createStackNavigator();
 
@@ -18,8 +24,38 @@ const App = () => {
           options={{headerShown:false}}
         />
         <Stack.Screen
-          name='Detail'
-          component={QuoteDetail}
+          name='Heart'
+          component={Heart}
+          options={{headerShown:false}}          
+        />
+        <Stack.Screen
+          name='Sun'
+          component={Sun}
+          options={{headerShown:false}}          
+        />
+        <Stack.Screen
+          name='Flower'
+          component={Flower}
+          options={{headerShown:false}}          
+        />
+         <Stack.Screen
+          name='Bird'
+          component={Bird}
+          options={{headerShown:false}}          
+        />
+        <Stack.Screen
+          name='Hand'
+          component={Hand}
+          options={{headerShown:false}}          
+        />
+        <Stack.Screen
+          name='Leaf'
+          component={Leaf}
+          options={{headerShown:false}}          
+        />
+        <Stack.Screen
+          name='Blue Flower'
+          component={BlueFlower}
           options={{headerShown:false}}          
         />
       </Stack.Navigator>
