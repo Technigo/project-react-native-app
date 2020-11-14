1251cabe0658fba8e3620b85bcf5fbd0a0ab1d47
MindstormingAB <s.elander@mindstorming.se>
2020-11-15 00:26:18 +0100

    Shake effect

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 import LottieView from 'lottie-react-native';
 import { Platform } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
 import Header from './components/Header';
 import Loading from './components/Loading';
@@ -19,6 +20,7 @@ const Container = styled.SafeAreaView`
 const App = () => {
   const [image, setImage] = useState();
   const [loading, setLoading] = useState();
+  const [accData, setAccData] = useState({});
 
   const generateNewImage = () => {
     setLoading(true);
@@ -29,8 +31,25 @@ const App = () => {
       .finally(() => setLoading(false));
   };
 
+  // useEffect(() => {
+  //   setLoading(true);
+  // }, []);
+
   useEffect(() => {
-    setLoading(true);
+    generateNewImage();
+  }, [accData]);
+
+  useEffect(() => {
+    Accelerometer.setUpdateInterval(100);
+    const listener = Accelerometer.addListener((data) => {
+      const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z);
+      if (totalForce > 3.5) {
+        setAccData(data)
+      }
+    });
+    return () => {
+      listener && listener.remove()
+    };
   }, []);
 
   return (
