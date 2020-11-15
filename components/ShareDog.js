c9378a8fdfbff2dac77caf050f8f075f1061837a
Ingela Löfgren <ingela.lofgren@gmail.com>
2020-11-15 13:22:55 +0100

    Last details before PR

--- components/ShareDog.js
+++ components/ShareDog.js
@@ -13,12 +13,12 @@ export const ShareDog = () => {
       });
       if (result.action === Share.sharedAction) {
         if (result.activityType) {
-          // shared with activity type of result.activityType
+          
         } else {
-          // shared
+         
         }
       } else if (result.action === Share.dismissedAction) {
-        // dismissed
+        
       }
     } catch (error) {
       alert(error.message);
