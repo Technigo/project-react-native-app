329109b8fbdbe2a8c58d1f59cbd6419fbdd8dd0b
Claudia Kjerrgren <ckjerrgren@gmail.com>
2020-11-15 18:15:46 +0100

    had to delete old version and replace with new one

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
-import React from 'react'
-import styled from 'styled-components/native'
+import React from 'react';
+import Main from './app/components/Main';
 
-const Container = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
-  align-items: center;
-`
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
 
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
-
-const App = () => {
-  return (
-    <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
-  )
+    );
+  }
 }
-
-export default App
---
+++ app/components/Note.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class Note extends React.Component {
+  render() {
+    return (
+
+      <View key={this.props.keyval} style={styles.note}>
+        <Text style={styles.noteText}>{this.props.val.date}</Text>
+        <Text style={styles.noteText}>{this.props.val.note}</Text>
+
+      <TouchableOpacity onPress={this.props.deleteMethod} style={styles.noteDelete} >
+        <Text style={styles.noteDeleteText}>🛒</Text>
+      </TouchableOpacity>
+
+      </View>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  note: {
+    position: 'relative',
+    padding: 20,
+    paddingRight: 100,
+    borderBottomWidth: 2,
+    borderBottomColor: '#263938',
+  },
+  noteText: {
+    paddingLeft: 20,
+    borderLeftWidth: 10,
+    borderLeftcolor: '#E91E63',
+  },
+  noteDelete: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    top: 10,
+    bottom: 10,
+    right: 10,
+  },
+  noteDeleteText: {
+    color: 'white',
+  },
+ 
+});
