759c853caf46690d9cf6945ea2cfa51d6a64da1e
Olof Lindahl <olindahl@gmail.com>
2020-11-15 12:55:17 +0100

    ready for submission

--- App.js
+++ App.js
@@ -28,6 +28,7 @@ const App = () => {
   const [data, setData] = useState(0);
   const [food, setFood] = useState({});
   const [section, setSection] = useState(0);
+  const [scans, setScans] = useState(0)
 
   useEffect(() => {
     (async () => {
@@ -36,31 +37,40 @@ const App = () => {
     })();
   }, []);
 
+  const handleBarCodeScanned = ({ type, data }) => {
+    console.log('barcodescanned')
+    setData(0);
+    setScanned(true);
+    setScans(scans + 1)
+    setData(data);
+    //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
   useEffect(() => {
     if (data !== 0) {
+      console.log('fetch')
+      setFood({})
       fetch(`https://world.openfoodfacts.org/api/v0/product/${data}`)
         .then((res) => res.json())
         .then((nutrient) => {
-          setFood(nutrient.product);
+          if(nutrient.product) setFood(nutrient.product);
+          else {alert('Product Not Found!')
+          //setScanned(false)
+        }
           //alert(`Food ${food.product_name} has been found!`)
           // alert(`Food ${nutrient.product.product_name} has been found!`)
           //setSection(2)
         });
     }
-  }, [data]);
+  }, [scans]);
 
   useEffect(() => {
     if (food.product_name) {
       //alert(`Food ${food.product_name} has been found!`);
+      console.log('test')
       setSection(2);
     }
   }, [food]);
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    setData(data);
-    //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -73,37 +83,35 @@ const App = () => {
     <>
       {section === 0 && (
         <Container>
-          <Title>This is your cool app! With a small little update</Title>
+          <Title>Simple Barcode Scanner App</Title>
           <Button title={"Tap to scan"} onPress={() => setSection(1)} />
-          <Title>💅💅💅</Title>
         </Container>
       )}
       {section === 1 && (
         <Container>
-          <Title>This is your cool app! With a small little update</Title>
+          {/* <Title>This is your cool app! With a small little update</Title> */}
           <BarCodeScanner
             onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
             style={StyleSheet.absoluteFillObject}
           />
-          {scanned && (
+          {(scanned && !food.product) && (
             <Button
               title={"Tap to Scan Again"}
               onPress={() => setScanned(false)}
             />
           )}
-
-          <Button
-            //onPress={onPressLearnMore}
-            title="Learn More"
-            color="#841584"
-            accessibilityLabel="Learn more about this purple button"
-          />
         </Container>
       )}
       {section === 2 && (
         <Container>
           <Title>Food {food.product_name} has been found!</Title>
-          <Button title={"Tap to scan again"} onPress={() => setSection(1)} />
+          <Button
+            title={"Tap to scan again"}
+            onPress={() => {
+              setScanned(false);
+              setSection(1);
+            }}
+          />
           <Title>💅💅💅</Title>
         </Container>
       )}
