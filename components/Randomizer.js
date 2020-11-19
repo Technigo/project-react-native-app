18873c61c95372078fdc3fabf678ef1219c662f3
Peggy <64912576+pwangy@users.noreply.github.com>
2020-11-19 12:33:21 +0100

    random villager picker work on button click

--- components/Randomizer.js
+++ components/Randomizer.js
@@ -1,57 +1,37 @@
 import React, { useState, useEffect } from 'react'
-import { View, Image } from 'react-native'
+import { View, Image, Button } from 'react-native'
 
-import villagers from '../villagers.json'
+import VillagerArray from './VillagerArray'
 import { BodyTextStyle } from './BodyTextStyle'
 import { Title } from './Title'
 
 
-
-const randomSelector = () => {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
-
-
-
 export const Randomizer = () => {
   const [villager, setVillager] = useState([])
-  console.log(villagers.x)
-  
-  // useEffect(() => {
-  //   // randomizer output determines index of object to be random villager
-  // }, [])
+  console.log(VillagerArray)
 
-  // needs object to hold random object variable?
-
+  const getVillager = () => {
+    const theVillager = VillagerArray[Math.floor(Math.random() * VillagerArray.length)]
+    setVillager(theVillager)
+  }
   return (
     <View>
-      <Title>{villagers.x[5].name}</Title>
+      <Title>{villager.name}</Title>
       {/* fix image link */}
-      <Image source={villagers.x[5].uri} />
+      <Image source={villager.uri} />
       {/* info text needs styling */}
-      <BodyTextStyle>{villagers.x[5].saying}</BodyTextStyle>
-      <BodyTextStyle>Personality: {villagers.x[5].personality}</BodyTextStyle>
-      <BodyTextStyle>Birthdate: {villagers.x[5].birthday}</BodyTextStyle>
-      <BodyTextStyle>Species: {villagers.x[5].species}</BodyTextStyle>
-      <BodyTextStyle>Gender: {villagers.x[5].gender}</BodyTextStyle>
-      <BodyTextStyle>Hobby: {villagers.x[5].hobby}</BodyTextStyle>
-      <BodyTextStyle>Catch Phrase: {villagers.x[5].catchPhrase}</BodyTextStyle>
+      <BodyTextStyle>{villager.saying}</BodyTextStyle>
+      <BodyTextStyle>Personality: {villager.personality}</BodyTextStyle>
+      <BodyTextStyle>Birthdate: {villager.birthday}</BodyTextStyle>
+      <BodyTextStyle>Species: {villager.species}</BodyTextStyle>
+      <BodyTextStyle>Gender: {villager.gender}</BodyTextStyle>
+      <BodyTextStyle>Hobby: {villager.hobby}</BodyTextStyle>
+      <BodyTextStyle>Catch Phrase: {villager.catchPhrase}</BodyTextStyle> 
 
-
+      <Button
+        onPress={() => { getVillager()}}>
+      Very Buttony Button
+      </Button>
     </View>
   )
 }
-
-{/* 
-const randomVillagerKeeper = {
-  key={id},
-  name={id.name.name-EUen},
-  image={id.icon_uri},
-  catchphrase={id.catch-phrase},
-  personality={id.personality},
-  hobby={id.hobby},
-  birthday={id.birthday-string},
-  saying={id.saying},
-}
-*/}
