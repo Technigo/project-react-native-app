e2e6f5f6f8441e162b65cfa1d3e37f97f89e1f2e
Sara Carlstein <carlstein.sara@gmail.com>
2021-04-06 19:57:21 +0200

    new layout

--- App.js
+++ App.js
@@ -12,14 +12,25 @@ const Container = styled.View`
 const Title = styled.Text`
   font-size: 24px;
   color: palevioletred;
-`;
+`
+
+const OracleImage = styled.Image`
+  width: 400px;
+  height: 400px;
+  margin-right: 50px;
+  `
 
 const App = () => {
   return (
     <Container>
+      <Title>Your daily affirmation card</Title>
+      <OracleImage source={require('./assets/fortune-card.png')} />
+      <Title>Shake me to shuffle the cards</Title>
       <SensorComponent></SensorComponent>
     </Container>
-  );
+  )
 };
 
 export default App;
+
+
