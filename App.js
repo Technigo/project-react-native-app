670a6bc5ae17c249b5ec271c88a16a18680fac57
rubmaigut <rubmaigut.7@gmail.com>
2021-04-07 19:21:18 +0200

    Navigation bar changed

--- App.js
+++ App.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { Feed } from './screens/Feed';
-import { Notifications } from './screens/Notifications';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { Home } from "./screens/Home";
+import { Upcoming } from "./screens/Upcoming";
+import { Profile } from "./screens/Profile";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Octicons, Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
 
-const Drawer = createDrawerNavigator();
+const tabBarStyle = {
+  style: {
+    backgroundColor: "black",
+  },
+  activeTintColor: "#e91e63",
+};
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Feed" component={Feed} />
-        <Drawer.Screen name="Notifications" component={Notifications} />
-      </Drawer.Navigator>
+      <Tab.Navigator initialRouteName="Home" tabBarOptions={tabBarStyle}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <Octicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Upcoming"
+          component={Upcoming}
+          options={{
+            tabBarLabel: "Upcoming",
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="new" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
