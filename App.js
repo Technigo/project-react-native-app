a7415778f77d7c22f47323781796c017fad47097
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-04-07 14:01:55 +0200

    Filters work only one at the time

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { Text } from 'react-native'
 import styled from 'styled-components/native'
 
 
-import {SEARCH_URL} from './reusables/urls'
-import  RecipeThumb  from './components/RecipeThumb'
+import { SEARCH_URL } from './reusables/urls'
+import RecipeThumb from './components/RecipeThumb'
 import SearchForm from './components/SearchForm'
 import Filters from './components/Filters'
 
@@ -12,47 +12,72 @@ import Filters from './components/Filters'
 const App = () => {
   const [recipes, setRecipes] = useState([])
   const [searchWord, setSearchWord] = useState('')
+  const [filteredRecipes, setFilteredRecipes] = useState([])
   const [search, setSearch] = useState('dinner')
   const [filter, setFilter] = useState(false)
+  const [glutenFilter, setGluteFilter] = useState(false)
+  const [calloriesFilter, setCalloriesFilter] = useState(false)
 
   const handleSubmit = () => {
     setSearch(searchWord)
   }
 
-  useEffect(()=> {
-    console.log(SEARCH_URL(search, filter))
+  useEffect(() => {
     fetch(SEARCH_URL(search, filter))
       .then(response => response.json())
       .then(receivedRecipes => setRecipes(receivedRecipes.hits))
-  },[search, filter])
+  }, [search, filter])
 
-  console.log(searchWord)
+  useEffect(() => {
+    setFilteredRecipes(recipes)
+    if (calloriesFilter) {
+      const newArray = filteredRecipes.filter((item) => {
+        const calories = Math.round((Number(item.recipe.calories) / Number(item.recipe.totalWeight)) * 100)
+        if (calories < 150) {
+          return item
+        }
+      })
+      setFilteredRecipes(newArray)
+    }
+    if (glutenFilter) {
+      const newArray = filteredRecipes.filter((item) => item.recipe.healthLabels.includes('Gluten-Free'))
+      setFilteredRecipes(newArray)
+      console.log(newArray)
+    }  
+  }, [calloriesFilter, glutenFilter, recipes])
+
+
   return (
     <Container>
       <Title>Choose your recipe here!</Title>
       <SearchForm
-        onSubmit={handleSubmit} 
-        searchWord={searchWord} 
+        onSubmit={handleSubmit}
+        searchWord={searchWord}
         setSearchWord={setSearchWord}
       />
-      <Filters 
+      <Filters
         filter={filter}
         setFilter={setFilter}
+        glutenFilter={glutenFilter}
+        setGluteFilter={setGluteFilter}
+        calloriesFilter={calloriesFilter}
+        setCalloriesFilter={setCalloriesFilter}
       />
-      {recipes.map((item) => <RecipeThumb key={item.recipe.uri} item={item} />)}      
+      {filteredRecipes.map((item) => <RecipeThumb key={item.recipe.uri} item={item} />)}
+
     </Container>
   )
 }
 
 export default App
 
-const Container = styled.ScrollView `
+const Container = styled.ScrollView`
   background-color: white;
   padding: 1px;
   padding-top: 30;
 `
 
-const Title = styled.Text `
+const Title = styled.Text`
   font-size: 24px;
   color: black;
   margin: 20px 0 0 5px;
--- components/Filters.js
+++ components/Filters.js
@@ -2,29 +2,58 @@ import React from 'react'
 import { Text } from 'react-native'
 import styled from 'styled-components/native'
 
-const Filters = ({filter, setFilter}) => {
+const Filters = ({filter, setFilter, glutenFilter, setGluteFilter, calloriesFilter, setCalloriesFilter}) => {
   const onPress = () => setFilter(!filter)
-  console.log(filter)
+  const glutenToggle = () => setGluteFilter(!glutenFilter)
+  const calloriesToggle = () => setCalloriesFilter(!calloriesFilter)
   return (
-   <> 
-    <Button 
-    filter={filter}
-    onPress={onPress}
-    >
+   <FiltersWrapper> 
+    <ButtonIngredients 
+      filter={filter}
+      onPress={onPress}>
       <Text>5 ingredients</Text>
-    </Button>
-  </> 
+    </ButtonIngredients>
+    <ButtonGluten
+      glutenFilter={glutenFilter}
+      onPress={glutenToggle}>
+      <Text>gluten-free</Text>
+    </ButtonGluten>
+
+    <ButtonCallories
+      calloriesFilter={calloriesFilter}
+      onPress={calloriesToggle}>
+      <Text> 150 callories</Text>
+    </ButtonCallories>
+
+  </FiltersWrapper> 
   )
 
 }
 export default Filters
+const FiltersWrapper = styled.View `
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  background: gray;
+`
 
 const Button = styled.TouchableOpacity`
   width: 120px;  
   height: 40px;
-  background-color: ${props => (props.filter? "#ffadad" : "#ccc")};
   border: none;
   font-size: 16px;
   border-radius: 20px;
   padding: 10px;
+  margin: 3px;
+  justify-content: center;
+  align-items:center;
+`
+const ButtonIngredients = styled(Button) `
+  background-color: ${props => (props.filter? "#6e8c6c" : "#ccc")};
+`
+const ButtonGluten = styled(Button) `
+  background-color: ${props => (props.glutenFilter? "#6e8c6c" : "#ccc")}
+`
+const ButtonCallories = styled(Button) `
+  background-color: ${props => (props.calloriesFilter? "#6e8c6c" : "#ccc")};
 `
