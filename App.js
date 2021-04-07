1ee47581c98d9c7c70999a52059d57a4df187470
Theresehag <tezzinthecity@gmail.com>
2021-04-08 00:56:27 +0200

    first rough test to create an app with fetch

--- App.js
+++ App.js
@@ -1,24 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components/native'
+import axios from 'axios'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
+  background-color: hotpink;
   justify-content: center;
   align-items: center;
 `
 
 const Title = styled.Text`
   font-size: 24px;
-  color: palevioletred;
+  margin: 5px;
+  color: black;
 `
 
+const SearchField = styled.TouchableOpacity`
+  width: 180;
+  height: auto;
+  padding: 10px
+  border: 2px solid black;
+  background-color: lightyellow;
+  margin: 40px;
+  
+  `
+
 const App = () => {
+  const [stats, setStats] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.quarantine.country/api/v1/summary/latest')
+    .then((response) => response.json ())
+    .then((json) => {
+      setStats(json.results) 
+    })
+    }, [])
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
+      <Title>Get the latest covid stats for Sweden!</Title>
+      <Title>Click below and get informed.</Title>
+      <Title>😷😷😷</Title>
+      <SearchField onClick = {fetchData}><Text>Sweden Covid stats</Text></SearchField>
+      <ScrollView>
+        {stats.map((stat) => (
+        <View key = {stat.regions.name}> 
+          <Title>Total cases: {stat.regions.sweden.total_cases} </Title>
+          <Title>Tested: {stat.regions.sweden.tested}</Title>
+          <Title>Deaths: {stat.regions.sweden.deaths}</Title>
+          <Title>Death ratio: {stat.regions.sweden.death_ratio}</Title>
+          </View>
+      ))}
+      </ScrollView>
     </Container>
   )
 }
