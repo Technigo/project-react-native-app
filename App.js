bb712a93045c622b1f7d6b3b8bab4491f39242de
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-04-07 14:56:05 +0200

    cleaned up a bit

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { Text } from 'react-native'
 import styled from 'styled-components/native'
 
 import { SEARCH_URL } from './reusables/urls'
