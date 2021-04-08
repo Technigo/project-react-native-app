519d9314ba24df5aa119b5867aac8eb6c0e2cf48
Hannah Jesinkey <h.jesinkey@gmail.com>
2021-04-08 20:21:52 +0200

    first page working

--- App.js
+++ App.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import styled from 'styled-components/native'
+import React, { useEffect, useState } from 'react';
+import { Image, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+
+// import CARD_API from './reusable/urls';
 
 const Container = styled.View`
   flex: 1;
   background-color: papayawhip;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Title = styled.Text`
   font-size: 24px;
   color: palevioletred;
-`
+`;
+
+const styles = StyleSheet.create({
+  image: {
+    width: 50,
+    height: 50,
+  },
+});
+
+const CardThumb = (props) => {
+  return (
+    <>
+      <Title>{props.name}</Title>
+      <Image style={styles.image} source={{ uri: props.image }} />
+    </>
+  );
+};
 
 const App = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.magicthegathering.io/v1/cards')
+      .then((res) => res.json())
+      .then((json) => setCards(json.cards));
+  }, []);
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
+      {cards.map((card) => (
+        <CardThumb key={card.id} name={card.name} image={card.imageUrl} />
+      ))}
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ reusable/urls.js
@@ -0,0 +1,1 @@
+export const CARD_API = 'https://api.magicthegathering.io/v1/cards';
