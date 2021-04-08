77f1e83ef5426af67ddcef34b06657c0e45db5ee
esteficodes <estefaniaquevedolusby@gmail.com>
2021-04-08 13:00:16 +0200

    did fetch for advices, rendered in App text

--- App.js
+++ App.js
@@ -1,5 +1,37 @@
-import React from 'react'
-import styled from 'styled-components/native'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components/native';
+
+import { API_URL } from './reusable/urls';
+
+
+
+const App = () => {
+  const [ advice, setAdvice] = useState([]);
+
+  useEffect(()=> {
+    fetchAdvice();
+  },);
+
+  const fetchAdvice = () => {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => setAdvice(data.slip.advice))
+    .catch(err => console.log(err)); 
+  }
+
+  return (
+    <Container>
+      <Title>Random Advice Giver</Title>
+      <Title>Your daily doses of unsolicited advice</Title>
+      <Text>{advice}</Text>
+      <Title>💅💅💅</Title>
+    </Container>
+  )
+}
+
+export default App
+
+/* Styling*/
 
 const Container = styled.View`
   flex: 1;
@@ -13,14 +45,7 @@ const Title = styled.Text`
   color: palevioletred;
 `
 
-const App = () => {
-  return (
-    <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
-  )
-}
-
-export default App
+const Text = styled.Text`
+  font-size: 24px;
+  color: palevioletred;
+`
---
+++ reusable/urls.js
@@ -0,0 +1,3 @@
+export const API_URL='https://api.adviceslip.com/advice';
+
+export default API_URL
