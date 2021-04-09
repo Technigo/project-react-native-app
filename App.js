0735556e6ee949cfed6fefb89c5b9f6f2f6e7e45
matilda svensson <info@matildasvensson.se>
2021-04-09 17:39:09 +0200

    Cleaned up the code and took away the things I was playing with before

--- App.js
+++ App.js
@@ -1,107 +1,21 @@
-import React, {useState} from 'react';
-import styled from 'styled-components/native';
-import { SensorComponent } from './components/SensorComponent';
-// import { Button } from './components/Button';
-import HeaderContainer from './components/HeaderContainer'
-import { Text, StyleSheet, Button, View, TouchableOpacity, ImageBackground} from 'react-native';
+import React from 'react'
+import styled from 'styled-components/native'
+import { SensorComponent } from './components/SensorComponent'
 
 const Container = styled.View`
   flex: 1;
   background-color: papayawhip;
   justify-content: center;
   align-items: center;
-`;
-// const HeaderContainer = styled.View`
-//   background-color: #222222;
-//   flex: 1;
-//   width: 100%;
-//   justify-content: center;
-//   align-items: center;
-//   `
-
-// const HeaderText = styled.Text`
-//   font-size: 14px;
-//   color: white;
-
-// `
-
-const BottomContainer = styled.View`
-  flex: 6;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`;
-
-const ImageContainer = styled.ImageBackground`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  background-color: #ff2255;
-  width: 100%;
-  height: 100%;
-`
-
-const MainText = styled.Text`
-  font-size: 80px;
-`
-const OrangeText = styled(MainText)`
-  color: #ff2255;
-`
-const CustomButton = styled.TouchableOpacity`
-  background-color: #ff2255;
-  justify-content: center;
-  align-items: center;
-  margin: 10px;
-  padding: 5px;
 `
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
-  const [imageUrl, setImageUrl] = useState('https://images.unsplash.com/photo-1517783755800-0344536f0ec5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1344&q=80')
-  const [name, onChangeName] = React.useState('')
-
   return (
     <Container>
-      {/* <HeaderContainer 
-        name={name}
-        onChangeName={onChangeName}
-        /> */}
-
-      <BottomContainer>
-
-      {/* <ImageContainer
-        source={{
-          uri: imageUrl,
-        }}
-        ></ImageContainer> */}
         <SensorComponent></SensorComponent>
-        {/* <Text style={styles.text}>Count: {count} </Text> */}
-        {/* <MainText>Hi</MainText>
-        <OrangeText>you {name}</OrangeText>
-        <Button
-          title="Press me"
-          onPress={() => setCount(count + 1)}
-        />
-        <CustomButton onPress={() => setCount(count + 1)}>
-          <Text>Try pressing here</Text>
-        </CustomButton> */}
-      </BottomContainer>
     </Container>
   );
 };
 
-export default App;
-
-
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 24,
-  }
-})
+export default App
--- components/SensorComponent.js
+++ components/SensorComponent.js
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Accelerometer } from 'expo-sensors';
-import styled from 'styled-components/native';
-
-
-
+import React, { useState, useEffect } from 'react'
+import { Accelerometer } from 'expo-sensors'
+import styled from 'styled-components/native'
 
 // ==========================
 // = Functions
@@ -11,11 +8,11 @@ const isShaking = (data) => {
   // x,y,z CAN be negative, force is directional
   // We take the absolute value and add them together
   // This gives us the total combined force on the device
-  const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z);
+  const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z)
 
   // If this force exceeds some threshold, return true, otherwise false
   // Increase this threshold if you need your user to shake harder
-  return totalForce > 1.2;
+  return totalForce > 1.2
 };
 
 // ==========================
@@ -26,7 +23,7 @@ const ShakeView = styled.View`
   justify-content: center;
   align-items: center;
   width: 100%;
-`;
+`
 
 const StartTitle = styled.Text`
 font-size: 36px;
@@ -53,20 +50,20 @@ const ShakeAlert = styled.Text`
   font-size: 36px;
   font-weight: bold;
   color: #EC3F59;
-`;
+`
 
 const ShakeDataView = styled.View`
-`;
+`
 
 const ShakeDataTitle = styled.Text`
   font-weight: bold;
   color: papayawhip;
   text-align: center;
-`;
+`
 const ShakeData = styled.Text`
 color: papayawhip;
 text-align: center;
-`;
+`
 
 export const SensorComponent = () => {
   // This function determines how often our program reads the accelerometer data in milliseconds
@@ -78,7 +75,7 @@ export const SensorComponent = () => {
     x: 0,
     y: 0,
     z: 0,
-  });
+  })
 
   // This keeps track of whether we are listening to the Accelerometer data
   const [subscription, setSubscription] = useState(null);
@@ -92,25 +89,24 @@ export const SensorComponent = () => {
         // The frequency of this function is controlled by setUpdateInterval
         setData(accelerometerData);
       })
-    );
-  };
+    )
+  }
 
   // This will tell the device to stop reading Accelerometer data.
   // If we don't do this our device will become slow and drain a lot of battery
   const _unsubscribe = () => {
     subscription && subscription.remove();
     setSubscription(null);
-  };
+  }
 
   useEffect(() => {
     // Start listening to the data when this SensorComponent is active
-    _subscribe();
+    _subscribe()
 
     // Stop listening to the data when we leave SensorComponent
     return () => _unsubscribe();
-  }, []);
+  }, [])
 
-  // const [recipeList, setRecipeList] = useState([])
   const [recipe, setRecipe] = useState('')
 
   let recipeArray = ['Pizza', 'Pasta', 'Soup', 'Sallad']
@@ -119,40 +115,11 @@ export const SensorComponent = () => {
     isShaking(data) && setRecipe(recipeArray[Math.floor(Math.random()*recipeArray.length)])
 }, [isShaking(data)])
 
-// useEffect(() => {
-//   if (isShaking(data) === true) {
-//     setRecipe(recipeArray[Math.floor(Math.random()*recipeArray.length)])
-// }, [isShaking(data)])
-// )}
-
-
-
-
-
   return (
     <ShakeView>
       <StartTitle>What should I eat?</StartTitle>
       <StartParagraph>Shake me to get a suggestion</StartParagraph>
-      {/* If isShaking returns true:
-        - We could render conditionally
-        - Maybe we want to dispatch some redux event when device shakes?
-        - Maybe change some styled props?  */}
-     
-      {/* {isShaking(data) === true && 
-        <ShakeAlert>{recipe}</ShakeAlert>} */}
-      
         <ShakeAlert>{recipe}</ShakeAlert>
-
-      {/* {isShaking(data) && <ShakeAlert>Shaking</ShakeAlert>} */}
-
-      {/* <ShakeDataView>
-        <ShakeDataTitle>Shake Data</ShakeDataTitle> */}
-        {/* toFixed(2) only shows two decimal places, otherwise it's quite a lot */}
-        {/* <ShakeData>X: {data.x.toFixed(2)}</ShakeData>
-        <ShakeData>Y: {data.y.toFixed(2)}</ShakeData>
-        <ShakeData>Z: {data.z.toFixed(2)}</ShakeData>
-      </ShakeDataView> */}
     </ShakeView>
-    
-  );
-};
+  )
+}
