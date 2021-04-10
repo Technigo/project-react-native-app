6e4910febebd7dd327312feef600d65779b37059
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-04-10 23:35:36 +0200

    API fetch

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled from 'styled-components/native'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
@@ -9,18 +9,47 @@ const Container = styled.View`
 `
 
 const Title = styled.Text`
-  font-size: 24px;
+  font-size: 20px;
   color: palevioletred;
 `
 
-const App = () => {
-  return (
-    <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
-  )
-}
+const ImageArt = styled.Image`
+  width: 50px;
+  height: 50px;
+`
 
-export default App
+const App = () => {
+  const ART_URL =
+    "https://www.rijksmuseum.nl/api/en/collection?key=b5cLQ2UN&involvedMaker=Rembrandt+van+Rijn&ps=1";
+  const [artList, setArtList] = useState();
+
+  useEffect(() => {
+    fetch(ART_URL)
+      .then((response) => response.json())
+      .then((json) => setArtList(json.artObjects[0]))
+  }, [])
+
+  
+
+  return (
+    <>
+    {artList ? console.log(artList.webImage.url) : console.log('not ready')}
+    {artList && (
+    <Container>
+      <Title>{artList.title}</Title>
+      <ImageCard
+        source={{
+          uri: artList.webImage.url
+        }} />
+    </Container>
+    )}
+    </>
+  );
+};
+
+export default App;
+
+const ImageCard = styled.Image`
+  width: 100%;
+  height: 400px;
+`
