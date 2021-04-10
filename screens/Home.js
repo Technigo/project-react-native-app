20a4929f7d8c7e523f81b7431e6a41a02950aa48
NoemiSalome <noemifrischknecht@macbook-pro.home>
2021-04-10 15:42:22 +0200

    new set up, check commit

---
+++ screens/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, Button } from 'react-native';
+import styled from 'styled-components/native';
+
+// This is the main container for this screen
+const FeedContainer = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+// The prop "navigation" is important if you are trying to open/toggle the drawer
+//  directly via Javascript
+export const Feed = ({ navigation }) => {
+  return (
+    <FeedContainer>
+      <Text>Hi there!</Text>
+      <Text>Are you wondering what the hack you are doing here? Well I'm wondering that too.</Text>
+      <Text>But since you're here you might as well check out some React Native Stuff I did. I mean look at that burger menu! 🤤</Text>
+      {/* Here is an example of how to open/toggle the drawer via javascript */}
+      <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
+      <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
+    </FeedContainer>
+  );
+};
