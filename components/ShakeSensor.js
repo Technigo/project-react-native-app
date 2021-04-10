2a95f1d84e236784038853f850989d4ae23c9822
Frivridna <54737644+Frivridna@users.noreply.github.com>
2021-04-10 13:34:27 +0200

    placed API call in ShakeSensor, updated README a little

--- components/ShakeSensor.js
+++ components/ShakeSensor.js
@@ -3,8 +3,9 @@ import { Accelerometer } from 'expo-sensors'
 import { useFonts, Inter_500Medium } from '@expo-google-fonts/inter'
 import AppLoading from 'expo-app-loading'
 
+import { MAGIC_API_single } from '../reusable/urls'
+
 import { ShakeView, ShakeAlert, ShakeDataView, InterShakeDataTitle, MagicBall } from '../StyledComponents/ShakeSensorStyling'
-import magicanswer from '../data/magicanswer'
 
 // FUNCTIONS
 // The total combined force on the device
@@ -15,21 +16,18 @@ const isShaking = (data) => {
   return totalForce > 1.78
 }
 
-const randomAnswer = (number) => {
-  return Math.floor(Math.random() * number)
-}
 // This function determines how often our program reads the accelerometer data in milliseconds
 // https://docs.expo.io/versions/latest/sdk/accelerometer/#accelerometersetupdateintervalintervalms 
 export const ShakeSensor = () => {
   Accelerometer.setUpdateInterval(400);
   
-  const [answer, setAnswer] = useState(undefined) // make answer false as default value
+  // Accelerometer returns three numbers (x,y,z) which represent the force currently applied to the device
+  const [data, setData] = useState({ x: 0, y: 0, z: 0, })
+  
+  // Keep track of whether we are listening to the Accelerometer data
+  const [subscription, setSubscription] = useState(null)
 
-  // The accelerometer returns three numbers (x,y,z) which represent the force currently applied to the device
-/*   const [data, setData] = useState({ x: 0, y: 0, z: 0, }) */
-
-  // This keeps track of whether we are listening to the Accelerometer data
-  const [subscription, setSubscription] = useState(null);
+  const [magicAnswer, setMagicAnswer] = useState({})// Tried setting useState to undefined, but got a black screen. 
 
   const _subscribe = () => {
     // Save the subscription so we can stop using the accelerometer later
@@ -37,10 +35,7 @@ export const ShakeSensor = () => {
       // This is what actually starts reading the data
       Accelerometer.addListener((accelerometerData) => {
         // Whenever this function is called, we have received new data
-        if (isShaking(accelerometerData)) {
-          const answerIndex = randomAnswer(magicanswer.length)
-          setAnswer(magicanswer[answerIndex])
-        }
+        setData(accelerometerData)
       })
     )
   }
@@ -56,8 +51,19 @@ export const ShakeSensor = () => {
     // Stop listening to the data when we leave SensorComponent
     return () => _unsubscribe()
   }, [])
+  
+  useEffect(() => {
+  isShaking(data) && fetch(MAGIC_API_single)
+    .then(res => res.json())
+    .then(magicdata => setMagicAnswer(magicdata))
+    .catch(err => console.error(err))
+  }, [isShaking(data)])
 
-  const [fontsLoaded] =useFonts({
+  if (magicAnswer === undefined) {
+    return <></>
+  }
+
+  const [fontsLoaded] = useFonts({
     'Inter_500Medium': require('../assets/fonts/Inter_500Medium.ttf')
   })
   if (!fontsLoaded) {
@@ -67,7 +73,7 @@ export const ShakeSensor = () => {
     <ShakeView>
       <ShakeDataView>
         <InterShakeDataTitle>
-          {answer ? "Didn't like the answer? Shake it up and try again!" : "Ask a question and shake me to get your magic answer!"}
+          {!magicAnswer ? "Didn't like the answer? Shake it up and try again!" : "Ask a question and shake me to get your magic answer!"}
         </InterShakeDataTitle>
         <MagicBall
           source={require('../assets/magic-ball.png')}
@@ -76,7 +82,7 @@ export const ShakeSensor = () => {
             resizeMode: 'cover',
           }}
         />
-        {answer ? <ShakeAlert>{answer}</ShakeAlert> : null}
+        {magicAnswer ? <ShakeAlert>{magicAnswer.Answer}</ShakeAlert> : <></>}
       </ShakeDataView>
     </ShakeView>
   )
--- reusable/urls.js
+++ reusable/urls.js
@@ -1,3 +1,1 @@
-//export const MAGIC_API = 'http://worm.amplitut.de:8080/completemagic'
-
 export const MAGIC_API_single = 'http://worm.amplitut.de:8080/magic'
