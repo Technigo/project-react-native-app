5b28fd311653754d7c83398350c6626ab98cf79d
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-04-10 12:22:58 +0200

    created reusable and components folder, fetched a joke Api, started with styling

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import React from 'react'
-import styled from 'styled-components/native'
 
-const Container = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  justify-content: center;
-  align-items: center;
-`
-
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
+import Jokes from './components/Jokes'
 
 const App = () => {
   return (
-    <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
-    </Container>
+    <Jokes />
   )
 }
 
---
+++ components/Jokes.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components/native'
+
+import { JOKE_API } from './reusable/url'
+
+const Container = styled.View`
+flex: 1;
+background-color: papayawhip;
+justify-content: center;
+align-items: center;
+`
+
+const Title = styled.Text`
+  font-size: 24px;
+  color: palevioletred;
+`
+
+const Main = styled.View`
+background-color: papayawhip;
+justify-content: center;
+align-items: center;
+padding: 30px ; 
+`
+
+const Button = styled.TouchableOpacity`
+width: 50%;
+padding: 10px
+border: 2px solid black;
+border-radius: 25px;
+height: 60px;
+background-color: grey;
+justify-content: center;
+align-items: center;
+`
+
+const ButtonContainer = styled.View`
+width: 100%;
+justify-content: center;
+align-items: center;
+`
+
+const Header = styled.View`
+background-color: grey
+justify-content: center;
+align-items: center;
+width: 100%;
+padding: 15px;
+margin-top: 60px;
+`
+
+const Jokes = () => {
+  const [houses, setHouses] = useState([])
+  const [updateJoke, setUpdateJoke] = useState(0)
+
+  const onUpdateJoke = () => {
+    setUpdateJoke(updateJoke + 1)
+  }
+
+  useEffect(() => {
+    fetch(JOKE_API)
+      .then(res => res.json())
+      .then(data => setHouses(data))
+  }, [updateJoke])
+
+  return (
+    <Container>
+      <Header>
+        <Title>Get a new superfunny joke everytime you press the button below!</Title>
+      </Header>
+      <Main>
+        <Title>{houses.joke}</Title>
+      </Main>
+      <ButtonContainer>
+        <Button onPress={onUpdateJoke}>
+          <Title>Press me</Title>
+        </Button>
+      </ButtonContainer>
+    </Container>
+  )
+}
+
+export default Jokes
