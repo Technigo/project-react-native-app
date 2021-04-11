721d6c3202ae13f348410aecc688e41d52ecdd6f
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-04-11 14:51:55 +0200

    Created basics for navigation in the app

--- App.js
+++ App.js
@@ -1,41 +1,19 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components/native";
-import ImageCard from './components/ImageCard'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const Container = styled.ScrollView`
-  background-color: black;
-  flex: 1;
-`
+import HomeScreen from "./components/HomeScreen";
 
-const Title = styled.Text`
-  font-size: 20px;
-  color: palevioletred;
-  flex: 1;
-`  
+const Stack = createStackNavigator();
 
 const App = () => {
-  const ART_URL =
-    "https://www.rijksmuseum.nl/api/en/collection?key=b5cLQ2UN&involvedMaker=Rembrandt+van+Rijn&ps=5";
-  const [artList, setArtList] = useState();
-
-
-  useEffect(() => {
-    fetch(ART_URL)
-      .then((response) => response.json())
-      .then((json) => setArtList(json.artObjects))
-  }, [])
 
   return (
-    <>
-    {artList && ( 
-    <Container>
-    {artList.map((artItem) =>  (
-      <ImageCard {...artItem} key={artItem.id} />
-     ))}
-     </Container>
-    )}
-   </>
-   
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ components/HomeScreen.js
@@ -0,0 +1,39 @@
+
+import React, { useEffect, useState } from "react";
+import styled from "styled-components/native";
+
+import ImageCard from "./ImageCard";
+
+const Container = styled.ScrollView`
+  background-color: black;
+  flex: 1;
+`;
+
+const HomeScreen = () => {
+
+  const ART_URL =
+    "https://www.rijksmuseum.nl/api/en/collection?key=b5cLQ2UN&involvedMaker=Rembrandt+van+Rijn&ps=5";
+  const [artList, setArtList] = useState();
+
+  useEffect(() => {
+    fetch(ART_URL)
+      .then((response) => response.json())
+      .then((json) => setArtList(json.artObjects));
+  }, []);
+
+
+  return (
+    <>
+    {artList && (
+      <Container>
+        {artList.map((artItem) => (
+          <ImageCard {...artItem} key={artItem.id} />
+        ))}
+      </Container>
+    )}
+    </>
+  )
+
+}
+
+export default HomeScreen;
