f4b47030111b95ccb7ffe67af08c4331f2682c9c
Maria Sparre <you@example.com>
2021-04-11 23:48:43 +0200

    removed View from App

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import React from "react"
 
-import {StyleSheet} from 'react-native'
 import { NavigationContainer } from "@react-navigation/native"
 import AppNavigator from "./navigation/AppNavigator"
 
 const App = () => {
   return (
-    <View style={styles.main}>
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
-    </View>
   )
 }
 
 export default App
-
-const styles = StyleSheet.create({
-  main: {
-  backgroundColor: "#F2F2F2" 
-  }
-})
