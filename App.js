817181b347798e45265b9584f842a0ae16a56ca4
“yennie <yennie.leek@hotmail.se>
2021-04-11 12:37:47 +0200

    added the styling for the nav

--- App.js
+++ App.js
@@ -6,26 +6,29 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { KanyeWest } from './screens/KanyeWest'
 import { RandomAdvice } from './screens/RandomAdvice'
 import styled from 'styled-components/native';
-import { View } from 'react-native';
+
 
 
 
 const Drawer = createDrawerNavigator();
 
-const MainNavigationContainer = styled(NavigationContainer)`
-  background-color: #f5d584;
-`
 
 const App = () => {
   return (
-    <MainNavigationContainer>
-        <Drawer.Navigator style={{backGroundColor:f5d584}}>
+    <NavigationContainer>
+          <Drawer.Navigator drawerContentOptions={{ 
+              activeBackgroundColor: '#7f8b7c',
+              labelStyle:{
+                color:'#20c5cb'
+              }}} 
+              drawerStyle={{backgroundColor: '#f5d584'}}
+          >
           <Drawer.Screen name="HOME" component={Feed} />
           <Drawer.Screen name="CHUCK NORRIS" component={ChuckNorris} />
           <Drawer.Screen name="KANYE WEST" component={KanyeWest} />
           <Drawer.Screen name="GUESS WHAT" component={RandomAdvice} />
         </Drawer.Navigator>
-    </MainNavigationContainer>
+    </NavigationContainer>
   );
 };
 
