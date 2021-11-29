6f7c742066a89897c0fc1a5b4b48c9e5f92c78f6
e-delsol <80672920+e-delsol@users.noreply.github.com>
2021-11-29 20:50:27 +0100

    Adding a button and some text just to try it out

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React, { useState } from "react";
+import styled from "styled-components/native";
+import { View, Text, Button } from "react-native";
 
 const Container = styled.View`
 	flex: 1;
@@ -14,11 +15,15 @@ const Title = styled.Text`
 `;
 
 const App = () => {
+	const [count, setCount] = useState(0);
+
 	return (
 		<Container>
-			<Title>This is your cool app!</Title>
+			<Title>React Native Project!</Title>
 			<Title>Go to App.js and start coding</Title>
 			<Title>💅💅💅</Title>
+			<Title>count: {count}</Title>
+			<Button title="Click me" onPress={() => setCount(count + 1)} />
 		</Container>
 	);
 };
