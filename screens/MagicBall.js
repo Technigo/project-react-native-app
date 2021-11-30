8c1874ac0bd882da8765fd1d229e49e4806129c3
JohannaMJ <johanna.mannestal@gmail.com>
2021-11-30 17:43:10 +0100

    trying out styled components in react native

--- screens/MagicBall.js
+++ screens/MagicBall.js
@@ -9,14 +9,11 @@ import {
 import styled from 'styled-components/native';
 import { Accelerometer } from 'expo-sensors';
 
-const QuoteText = styled.Text`
-  font-weight: 700;
-`;
-
 export const MagicBall = () => {
   const [magicAnswer, setMagicAnswer] = useState('');
-
-  const [text, onChangeText] = useState('Useless Text');
+  const [question, setQuestion] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [subscription, setSubscription] = useState(null);
 
   const [data, setData] = useState({
     x: 0,
@@ -25,9 +22,6 @@ export const MagicBall = () => {
   });
 
   const answers = ['yes', 'maybe', 'no'];
-
-  const [loading, setLoading] = useState(false);
-  const [subscription, setSubscription] = useState(null);
 
   useEffect((answer) => {
     generateMagicBall(answer);
@@ -63,12 +57,17 @@ export const MagicBall = () => {
 
   const generateMagicBall = () => {
     setLoading(true);
-    Vibration.vibrate();
     const answer = answers[Math.floor(Math.random() * answers.length)];
     setMagicAnswer(answer);
     console.log(answer);
-    setTimeout(() => setLoading(false), 2000);
+    Vibration.vibrate();
+    setTimeout(() => setLoading(false), 1000);
+    setQuestion('');
   };
+
+  // const onQuestionChange = (event) => {
+  //   setQuestion(event.target.value);
+  // };
 
   const isShakingEnough = (data) => {
     const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z);
@@ -80,14 +79,53 @@ export const MagicBall = () => {
   }
 
   return (
-    <View>
-      <TextInput
+    <MagicBallContainer>
+      <InputField
         // style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder='useless placeholder'
+        // onChangeText={onQuestionChange}
+        // value={question}
+        placeholder='Your question to the magic ball'
       />
-      <QuoteText>Answer: {magicAnswer}</QuoteText>
-    </View>
+      <StyledBall>
+        <MagicText>Answer: {magicAnswer}</MagicText>
+      </StyledBall>
+    </MagicBallContainer>
   );
 };
+
+const MagicBallContainer = styled.View`
+  background-color: antiquewhite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const InputField = styled.TextInput`
+  height: 50px;
+  width: 75%;
+  border: 2px solid #000;
+  margin: 10px;
+  padding: 5px;
+`;
+
+const StyledBall = styled.View`
+  height: 300px;
+  width: 300px;
+  background-color: #000;
+  z-index: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 150px;
+`;
+
+const MagicText = styled.Text`
+  font-weight: 700;
+  font-size: 30px;
+  border: 1px solid #000;
+  width: 50%;
+  padding: 5px;
+  z-index: 1;
+  background-color: aliceblue;
+`;
