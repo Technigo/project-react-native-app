94305e6c789411d15c9cc7a3520eff1751d254fa
Charlotte Nyman <85078175+Svempolin@users.noreply.github.com>
2021-11-30 15:23:58 +0100

    starting with the simulator

--- components/ShakeApi.js
+++ components/ShakeApi.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, TouchableOpacity, ActivityIndicator } from "react-native";
+import { Accelerometer } from 'expo-sensors';
 import styled from "styled-components/native";
 
 const QuoteText = styled.Text`
@@ -18,6 +19,57 @@ const ButtonApi = () => {
   useEffect(() => {
     generateActivity();
   }, []);
+
+
+
+
+
+
+
+export default function App() {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const _slow = () => {
+    Accelerometer.setUpdateInterval(1000);
+  };
+
+  const _fast = () => {
+    Accelerometer.setUpdateInterval(16);
+  };
+
+  const _subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener(accelerometerData => {
+        setData(accelerometerData);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+
+
+
+
+
+
+
+
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+
 
   const generateActivity = () => {
     setLoading();
@@ -43,13 +95,3 @@ const ButtonApi = () => {
 };
 
 export default ButtonApi;
-
-//  const generateArt = () => {
-//     fetch("https://api.artic.edu/api/v1/artworks")
-//       .then((res) => res.json())
-//       .then((data) => setArt(data));
-//   };
-
-// setTimeout(function(){
-//  	alert("Sup!");
-// }, 2000);
