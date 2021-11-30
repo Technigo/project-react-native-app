9b83854f47a7fd78babfdf8866d12f681877d511
louise-becker <louise.becker@gmail.com>
2021-11-30 14:20:00 +0100

    navigation added, camera initiated as one of the pages

--- App.js
+++ App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import styled from 'styled-components/native';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CameraPage from './components/CameraPage';
+	
 
-const Container = styled.View`
-	flex: 1;
-	background-color: papayawhip;
-	justify-content: center;
-	align-items: center;
-`;
+	function HomeScreen({ navigation }) {
+		return (
+		  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Home Screen</Text>
+			<Button
+			  title="Go to Details"
+			  onPress={() => navigation.navigate('Details')}
+			/>
+		  </View>
+		);
+	  }
 
-const Title = styled.Text`
-	font-size: 24px;
-	color: palevioletred;
-`;
+	  function DetailsScreen({ navigation }) {
+		return (
+		  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Details Screen</Text>
+			<Button
+			  title="Go to Camera"
+			  onPress={() => navigation.navigate('Camera')}
+			/>
+			<Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+			{/* <Button title="Go back" onPress={() => navigation.goBack()} />
+			<Button
+			  title="Go back to first screen in stack"
+			  onPress={() => navigation.popToTop()}
+			/> */}
+		  </View>
+		);
+	  }
 
-const App = () => {
-	return (
-		<Container>
-			<Title>This is your cool app!</Title>
-			<Title>Go to App.js and start coding</Title>
-			<Title>💅💅💅</Title>
-		</Container>
-	);
-};
-
-export default App;
+	  function CameraScreen({ navigation }) {
+		return (
+			<>
+			<CameraPage/>
+			</>
+		)
+	  } 
+	
+	const Stack = createNativeStackNavigator();
+	
+	const App = () => {
+		return (
+			<NavigationContainer>
+			  <Stack.Navigator initialRouteName="Home">
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Details" component={DetailsScreen} />
+				<Stack.Screen name="Camera" component={CameraScreen} />
+			  </Stack.Navigator>
+			</NavigationContainer>
+		  );
+		}
+	
+	export default App;
+	
