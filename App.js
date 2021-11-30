baebf1969104930cdefdc22cacf2b51f1f4f7a36
jakobxlindstrom <jake@lindstroem.se>
2021-11-30 21:33:08 +0100

    inital

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React from 'react'
+import styled from 'styled-components/native'
+import API from './components/API'
 
 const Container = styled.View`
-	flex: 1;
-	background-color: papayawhip;
-	justify-content: center;
-	align-items: center;
-`;
+  flex: 1;
+  background-color: red;
+  justify-content: center;
+  align-items: center;
+`
 
 const Title = styled.Text`
-	font-size: 24px;
-	color: palevioletred;
-`;
+  font-size: 24px;
+  color: white;
+`
 
 const App = () => {
-	return (
-		<Container>
-			<Title>This is your cool app!</Title>
-			<Title>Go to App.js and start coding</Title>
-			<Title>💅💅💅</Title>
-		</Container>
-	);
-};
+  return (
+    <Container>
+      <API />
+      <Title>This is your cool app!</Title>
+      <Title>Go to App.js and start coding</Title>
+      <Title>💅💅💅</Title>
+    </Container>
+  )
+}
 
-export default App;
+export default App
---
+++ components/API.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, ActivityIndicator } from 'react-native'
+import styled from 'styled-components/native'
+import { Accelerometer } from 'expo-sensors'
+
+const QuoteText = styled.Text`
+  font-weight: 700;
+`
+
+const API = () => {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  })
+  const [quote, setQuote] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [subscription, setSubscription] = useState(null)
+
+  useEffect(() => {
+    generateQuote()
+  }, [])
+
+  useEffect(() => {
+    Accelerometer.setUpdateInterval(1000)
+    subscribe()
+    return () => unsubscribe()
+  }, [])
+
+  useEffect(() => {
+    if (isShakingEnough(data)) {
+      generateQuote()
+    }
+  }, [data])
+
+  const subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener((accelerometerData) => {
+        setData(accelerometerData)
+      })
+    )
+  }
+
+  const unsubscribe = () => {
+    subscription && subscription.remove()
+    setSubscription(null)
+  }
+
+  const generateQuote = () => {
+    setLoading(true)
+    fetch('http://api.quotable.io/random')
+      .then((res) => res.json())
+      .then((data) => setQuote(data))
+      .finally(() => setLoading(false))
+  }
+
+  const isShakingEnough = (data) => {
+    const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z)
+    return totalForce > 1.78
+  }
+
+  if (loading) {
+    return <ActivityIndicator />
+  }
+
+  return (
+    <View>
+      <QuoteText>Quote: {quote.content}</QuoteText>
+      <Text>Author: {quote.author}</Text>
+    </View>
+  )
+}
+
+export default API
