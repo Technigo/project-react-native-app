5e2136d1add748aa3d0e467c3461926446fec6e2
Skrosen <76884712+Skrosen@users.noreply.github.com>
2021-12-01 17:32:24 +0100

    fixed styling that gave error in expo app

--- App.js
+++ App.js
@@ -1,33 +1,40 @@
 import React from "react";
 import styled from "styled-components/native";
+import { ImageBackground, StyleSheet } from "react-native";
 // import { createDrawerNavigation } from ''
 // import {Navigation } from ''
 
 import ButtonApi from "./components/ButtonApi";
 
-const Container = styled.View`
-	flex: 1;
-	background-color: grey;
-	background-image: url("https://images.unsplash.com/photo-1515255384510-23e8b6a6ca3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80");
-	background-repeat: no-repeat;
-	background-size: cover;
-	justify-content: center;
-	align-items: center;
-`;
+const mapImage = {
+	uri: "https://images.unsplash.com/photo-1515255384510-23e8b6a6ca3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+};
 
-const Title = styled.Text`
-	font-size: 24px;
-	color: rgba(245, 222, 179, 0.8);
-	text-shadow: 2px 2px 8px #000000;
-`;
+const styles = StyleSheet.create({
+	background: {
+		flex: 1,
+		backgroundColor: "grey",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+
+	title: {
+		fontSize: 24,
+		color: "rgba(245, 222, 179, 0.8)",
+	},
+});
 
 const App = () => {
 	return (
-		<Container>
+		<ImageBackground
+			source={mapImage}
+			resizeMode="cover"
+			style={styles.background}
+		>
 			{/* ev ta in så man har två flikar och kan visa knapp med citat och bilder med skak */}
-			{/* <Title>On average, these people know more than John Snow</Title> */}
+			{/* <Title style={styles.title}>On average, these people know more than John Snow</Title> */}
 			<ButtonApi />
-		</Container>
+		</ImageBackground>
 	);
 };
 
--- components/ButtonApi.js
+++ components/ButtonApi.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import {
 	View,
+	ScrollView,
 	Text,
 	TouchableOpacity,
 	ActivityIndicator,
@@ -28,19 +29,19 @@ const ButtonApi = () => {
 	}
 
 	return (
-		<View style={styled.quoteHeader}>
-			<View style={styled.quoteContainer}>
+		<ScrollView style={styled.quoteHeader}>
+			<ScrollView style={styled.quoteContainer}>
 				<Text style={styled.quoteText}>"{quote.sentence}"</Text>
 				{/* lägga till att if house = null return something */}
 				{/* kanske också if name is john snow, eller slugen? generateQuote igen för att ej visa hans citat */}
 				<Text style={styled.nameAndHouse}>
 					- {quote.character?.name}, {quote.character?.house?.name}
 				</Text>
-			</View>
+			</ScrollView>
 			<TouchableOpacity onPress={generateQuote}>
 				<Text style={styled.button}>Generate GOT wisdom</Text>
 			</TouchableOpacity>
-		</View>
+		</ScrollView>
 	);
 };
 export default ButtonApi;
@@ -49,46 +50,35 @@ const styled = StyleSheet.create({
 	quoteHeader: {
 		padding: 10,
 		margin: 0,
-		width: 700,
-		minWidth: 600,
-		maxWidth: 800,
+		minWidth: 150,
+		maxWidth: 200,
 		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
 	},
 
 	quoteContainer: {
 		backgroundColor: "rgba(245,222,179, 0.5)",
-		boxShadow: "2px 2px 8px #DEB887",
 		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
 		borderRadius: 2,
 		padding: 10,
 	},
 
 	quoteText: {
 		color: "rgba(51, 34, 21, 0.9)",
-		textShadow: "2px 2px 8px #DEB887",
 		fontSize: 30,
 		fontStyle: "italic",
 		display: "flex",
-		textAlign: "center",
 		padding: 5,
 	},
 
 	nameAndHouse: {
 		color: "rgba(51, 34, 21, 0.9)",
-		textShadow: "2px 2px 8px #DEB887",
 	},
 
 	button: {
 		backgroundColor: "rgba(245,222,179, 0.8)",
 		padding: 5,
-		width: "fit-content",
 		margin: 5,
 		borderRadius: 5,
 		color: "rgba(51, 34, 21, 1)",
-		textShadow: "2px 2px 8px #DEB887",
 	},
 });
