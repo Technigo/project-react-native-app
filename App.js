a7621a2cac81d955b27924acf1b3fbbbbed31697
dandeloid <82587220+dandeloid@users.noreply.github.com>
2021-12-01 22:27:29 +0100

    added startscreen

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
-import { View, Text } from "react-native"
+import { View, Text, Image, Button } from "react-native"
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
 // import ButtonApi from './components/ButtonApi';
 import Header from './components/Header'
+import Home from './components/Home'
 import ShakeFox from './components/ShakeFox'
 import ShakeDog from './components/ShakeDog'
 import ShakeCat from './components/ShakeCat'
 
-/* const Container = styled.View`
+const Container = styled.View`
 	flex: 1;
 	background-color: papayawhip;
 	justify-content: center;
@@ -21,7 +22,18 @@ import ShakeCat from './components/ShakeCat'
 const Title = styled.Text`
 	font-size: 22px;
 	color: palevioletred;
-`; */
+`;
+
+const StartButton = styled.TouchableOpacity`
+	width: 100px;
+    margin: 20px;
+	padding: 10px;
+	border-radius: 10px;
+    background-color: orange;
+`
+const EnterText = styled.Text`
+	text-align: center;
+`
 
 const Drawer = createDrawerNavigator()
 
@@ -33,15 +45,51 @@ const App = () => {
 		</Container>
 	); */
 
+	/* const homeonOn = true */
+	const [onHome, setOnHome] = useState(true)
+
 	return (
-		
+		<>
+		{onHome ? 
+		(<Container>
+			<Title>Welcome to the Animal Shaker</Title>
+			<StartButton onPress={() => setOnHome(false)}><EnterText>Enter</EnterText></StartButton>
+		</Container>) 
+		: (
 		<NavigationContainer>
-			<Drawer.Navigator initialRouteName="Foxes">
-				<Drawer.Screen name="Foxes" component={ShakeFox} />
-				<Drawer.Screen name="Cats" component={ShakeCat} />
-				<Drawer.Screen name="Dogs" component={ShakeDog} />
+			<Drawer.Navigator initialRouteName="Home">
+				
+				<Drawer.Screen name="Foxes" component={ShakeFox} 
+					options={{		
+						drawerIcon: ({ }) => (
+						<Image
+							source={require('./assets/fox-icon.svg')}
+							style={{ height: 15, width: 15 }}
+						/>
+					)}}
+				/>
+				<Drawer.Screen name="Dogs" component={ShakeDog}
+					options={{		
+						drawerIcon: ({ }) => (
+						<Image
+							source={require('./assets/dog-icon.svg')}
+							style={{ height: 15, width: 15 }}
+						/>
+					)}}
+				/>
+				<Drawer.Screen name="Cats" component={ShakeCat}
+					options={{		
+						drawerIcon: ({ }) => (
+						<Image
+							source={require('./assets/cat-icon.svg')}
+							style={{ height: 15, width: 15 }}
+						/>
+					)}}
+				/>
 		  	</Drawer.Navigator>
 		</NavigationContainer>
+		)}
+	</>
 	  )
 
 };
--- components/ShakeFox.js
+++ components/ShakeFox.js
@@ -1,22 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ActivityIndicator, Image  } from 'react-native';
+import { View, Text, ActivityIndicator, Image, ImageBackground  } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 import styled from 'styled-components/native'
 
+// styled components
 const AnimalContainer = styled.View`
     border-radius: 10px;
     padding: 20px;
-    margin: 20px 10%;
-    background-color: lightgrey;
+    margin: 0 10%;
+    /* background-color: lightgrey; */
     justify-content: center;
     align-items: center;
 `
-const TitleText = styled.Text`
-    margin: 10px 0 20px 0;
-    color: grey;
+const TextContainer = styled.View`
+    background-color: orange;
+    margin: 10px 0 10px 0;
+    width: 280px;
+    padding: 16px;
+    border-radius: 10px;
+    justify-content: center;
 `
+const TitleText = styled.Text`
+    text-align: center;
+    color: white;
+`
+const BottomText = styled.Text`
+    text-align: center;
+    color: white;
+`
+
+// API URL
 const RandomFox = 'https://randomfox.ca/floof/' //.image
 
+// Shake function
 const ShakeFox = () => {
   const [data, setData] = useState({
     x: 0,
@@ -56,6 +72,12 @@ const ShakeFox = () => {
     setSubscription(null);
   };
 
+  const isShakingEnough = (data) => {
+    const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z)
+    return totalForce > 1.78
+  }
+
+  // Fetch random animal
   const generateAnimal = () => {
       setLoading(true)
       fetch(RandomFox)
@@ -64,19 +86,17 @@ const ShakeFox = () => {
         .finally(() => setLoading(false))
   }
 
-  const isShakingEnough = (data) => {
-      const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z)
-      return totalForce > 1.78
-  }
-
   return (
+    <ImageBackground source={require('../assets/pixel-heart.png')} style={{width: '100%', height: '100%'}} imageStyle={{resizeMode: 'repeat'}}>
       <AnimalContainer>
-            <TitleText>Random Foxes!</TitleText>
+            <TextContainer><TitleText>Random Foxes!</TitleText></TextContainer>
             {loading && <ActivityIndicator size="large" color="blue" />}
             <Image source = {{uri: animal.image}}
-            style = {{ width: 200, height: 200, borderRadius: 10, margin: 10}}
+            style = {{ width: 280, height: 280, borderRadius: 10, margin: 10}}
             />
+            <TextContainer><BottomText>Shake for a new fox</BottomText></TextContainer>
       </AnimalContainer>
+      </ImageBackground>
     )
   }
 
