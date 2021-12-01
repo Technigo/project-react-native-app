40d0ff2391b0576619fd60eb66d3ae020617a106
Maria Thomasson <81348201+MariaThomasson@users.noreply.github.com>
2021-12-01 10:46:03 +0100

    Set up after monday lecture

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
+import ButtonApi from './components/ButtonApi';
+// import ShakeApi from './components/ShakeApi';
+
 const Container = styled.View`
 	flex: 1;
 	background-color: papayawhip;
@@ -16,9 +19,8 @@ const Title = styled.Text`
 const App = () => {
 	return (
 		<Container>
-			<Title>This is your cool app!</Title>
-			<Title>Go to App.js and start coding</Title>
-			<Title>💅💅💅</Title>
+      <ButtonApi />
+			{/* <ShakeApi /> */}
 		</Container>
 	);
 };
