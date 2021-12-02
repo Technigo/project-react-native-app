9789ff814830c418ac5808178678ba907eb8c322
louise-becker <louise.becker@gmail.com>
2021-12-02 22:58:54 +0100

    styling

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { Pressable, Button, View, Text } from 'react-native';
+import { Pressable, Button, View, Text, ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CameraPage from './components/CameraPage';
 import styled from 'styled-components/native';
 import ApiFetch from './components/APIFetch';
-	
+import Quotes from './components/Quotes';
+import { useFonts, Princess_Sofia } from '@expo-google-fonts/princess-sofia';
+
+const image = { uri: "https://images.pexels.com/photos/4021523/pexels-photo-4021523.jpeg?auto=compress&cs=tiny" };
+
+// const fonts = useFonts({
+//     Princess_Sofia,
+//   });
+
 const HomeContainer = styled.View`
 	flex: 1;
-	background-color: salmon;
+	background-color: darkslateblue;
 	justify-content: center;
 	/* align-items: center; */
 `;
 const Title = styled.Text`
-	font-size:35px;
-	color: lightgoldenrodyellow;
+	/* font-size:70px;	 */
+	color: whitesmoke;
 	padding: 10px;
+	text-shadow: 2px 2px;
+	text-align: center;
+	/* font-family: princess_sofia; */
 `;
 
 const GoToDetailsButton = styled.Pressable`
@@ -25,27 +36,41 @@ const GoToDetailsButton = styled.Pressable`
     text-transform: uppercase;
     letter-spacing: 2.5px;
     font-weight: 700;
-    color: white;
-    background-color: peachpuff;
+    background-color: slateblue;
     border: none;
     border-radius: 45px;
     box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
     transition: all 0.3s ease 0s;
     cursor: pointer;
     outline: none;
-    margin: 10px 100px 10px 100px;
+    margin: 30px 100px 30px 100px;
 `
-const DetailsContainer = styled.View`
+	const ButtonTextHome = styled.Text`
+		color: white;
+		font-weight: 600;
+		font-size: 22px;
+		/* text-shadow: 1px 1px; */
+`
+const ImageBackgroundDetails = styled.ImageBackground`
 	flex: 1;
-	background-color: salmon;
 	justify-content: center;
-	/* align-items: center; */
 `;
 
 const TitleDetails = styled.Text`
 	font-size:35px;
-	color: lightgoldenrodyellow;
+	/* color: lightgoldenrodyellow; */
 	padding: 10px;
+    color: white;
+	line-height: 30px;
+    font-weight: bold;
+    text-align: center;
+    background-color: #483D8Bc0;
+	object-fit: cover;
+
+	.span {
+		page-break-before: always;
+		/* font-style: italic; */
+	}
 `;
 
 const GoToCameraButton = styled.Pressable`
@@ -56,7 +81,7 @@ const GoToCameraButton = styled.Pressable`
     letter-spacing: 2.5px;
     font-weight: 700;
     color: white;
-    background-color: peachpuff;
+    background-color: aqua;
     border: none;
     border-radius: 45px;
     box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
@@ -74,7 +99,7 @@ const HomeButton = styled.Pressable`
     letter-spacing: 2.5px;
     font-weight: 700;
     color: white;
-    background-color: peachpuff;
+    background-color: aqua;
     border: none;
     border-radius: 45px;
     box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
@@ -82,27 +107,30 @@ const HomeButton = styled.Pressable`
     cursor: pointer;
     outline: none;
     margin: 10px 100px 10px 100px;
+	alignItems: center;
 `
 
 	function HomeScreen({ navigation }) {
 		return (
 			<HomeContainer>
+				<ImageBackground source={image} resizeMode="cover">
 				 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Title>Ready to Rock n Roll?</Title>
+			 <Title style={{ fontFamily: 'princess-sofia', fontSize: 35 }}>Ready to Cinderella?</Title>
 			<GoToDetailsButton
 			 onPress={() => navigation.navigate('Details')}>
-			 <Text> Yes! </Text>
+			 <ButtonTextHome> Yes! </ButtonTextHome>
 			</GoToDetailsButton>
 		  </View>
+		  </ImageBackground>
 		  </HomeContainer>
 		);
 	  }
 
 	  function DetailsScreen({ navigation }) {
 		return (
-		  <DetailsContainer>
-			  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<TitleDetails>Details Screen</TitleDetails>
+		  <ImageBackgroundDetails source={image} resizeMode="cover">
+			  <View style={{ flex: 1, justifyContent: 'center' }}>
+			<TitleDetails>{Quotes[Math.floor(Math.random() * Quotes.length)]} <span> ― Cinderella</span> </TitleDetails>
 			<GoToCameraButton
 			  onPress={() => navigation.navigate('Camera')}>
 			<Text>
@@ -120,7 +148,7 @@ const HomeButton = styled.Pressable`
 			  onPress={() => navigation.popToTop()}
 			/> */}
 		  </View>
-		  </DetailsContainer>
+		  </ImageBackgroundDetails>
 		);
 	  }
 
@@ -135,7 +163,7 @@ const HomeButton = styled.Pressable`
 	const Stack = createNativeStackNavigator();
 	
 	const App = () => {
-		return (
+			  return (
 			<NavigationContainer>
 			  <Stack.Navigator initialRouteName="Home">
 				<Stack.Screen name="Home" component={HomeScreen} />
---
+++ components/Quotes.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Quotes =
+[ 'Perhaps the greatest risk any of us will ever take is to be seen as we really are.',
+'Be kind, have courage and always believe in a little magic.',
+'A dream is a wish your heart makes.',
+'Where this is kindness, there is goodness. And where this goodness, there is magic.',
+'Have courage and be kind.',
+'No matter how your heart is grieving, if you keep on believing, the dream that you wish can come true.',
+'A dream is a wish your heart makes when you´re fast asleep.',
+'I want to tell you a secret that will see you through all the trials that life can offer.',
+'One shoe can change your life.'
+]
+
+export default Quotes
