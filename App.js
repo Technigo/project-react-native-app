605ccf28d2ed9985c2e572bcc06f1c23569fe4bf
Maria Petersson <mariapetersson@Marias-MacBook.lan>
2021-12-02 19:22:33 +0100

    test

--- App.js
+++ App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
-	flex: 1;
-	background-color: papayawhip;
-	justify-content: center;
-	align-items: center;
+  flex: 1;
+  background-color: papayawhip;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Title = styled.Text`
-	font-size: 24px;
-	color: palevioletred;
+  font-size: 24px;
+  color: hotpink;
 `;
 
 const App = () => {
-	return (
-		<Container>
-			<Title>This is your cool app!</Title>
-			<Title>Go to App.js and start coding</Title>
-			<Title>💅💅💅</Title>
-		</Container>
-	);
+  return (
+    <Container>
+      <Title>This is your cool app!</Title>
+      <Title>Go to App.js and start coding</Title>
+      <Title>💅💅💅</Title>
+    </Container>
+  );
 };
 
 export default App;
