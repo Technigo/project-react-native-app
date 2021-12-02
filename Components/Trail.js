94ae4dad7007256501f647dd69037c8508ac6d71
HedvigM <hedvig@mejstedt.se>
2021-12-02 20:28:09 +0100

    random trail

--- Components/Trail.js
+++ Components/Trail.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import styled from 'styled-components/native';
 import { Accelerometer } from 'expo-sensors';
+import trails from '../.expo-shared/trails.json';
 
 export const Trail = () => {
   const [subscription, setSubscription] = useState(null);
-
+  const [loading, setLoading] = useState(false);
   const currentPosition = useSelector((store) => store.trails.currentPosition);
+
+  const currentTrails = trails[currentPosition];
+  console.log(currentTrails.length);
 
   const [data, setData] = useState({
     x: 0,
@@ -17,10 +21,8 @@ export const Trail = () => {
 
   //This is seting a trail to start with. Figure out if want that to be set in the begining or not.
   useEffect(() => {
-    if (isShakingEnough(data)) {
-      /* generateQuote(); */
-    }
-  }, [data]);
+    generateTrail();
+  }, []);
 
   useEffect(() => {
     Accelerometer.setUpdateInterval(1000);
@@ -30,12 +32,7 @@ export const Trail = () => {
 
   useEffect(() => {
     if (isShakingEnough(data)) {
-      /* generateQuote(); */
-      return (
-        <View>
-          <Title>Hej</Title>
-        </View>
-      );
+      generateTrail();
     }
   }, [data]);
 
@@ -52,10 +49,21 @@ export const Trail = () => {
     setSubscription(null);
   };
 
+  const generateTrail = () => {
+    setLoading(true);
+
+    // En trail skall slumpas fram utifrån vilken av knapparna man tryckte på i första steget.
+    /*  .finally(() => setLoading(false)); */
+  };
+
+  const randomTrailNumber = Math.floor(Math.random() * currentTrails.length);
+
   const isShakingEnough = (data) => {
     const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z);
     return totalForce > 1.78;
   };
+
+  const randomTrail = currentTrails[randomTrailNumber];
 
   return (
     <View>
@@ -63,6 +71,10 @@ export const Trail = () => {
         You are in the {currentPosition} of Sweden, shake for a suggestion on a
         trail!
       </Title>
+      <View>
+        <Text>Trail name: {randomTrail.trail}</Text>
+        <Text>Trail description: {randomTrail.description}</Text>
+      </View>
     </View>
   );
 };
