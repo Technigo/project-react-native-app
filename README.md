ad0d392b2ed817b13c0b83f49ba6994eca64c338
Reinis <reinis@Reiniss-MacBook-Pro.local>
2021-12-03 15:18:29 +0100

    2nd component styled, Read me updated

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # Project React Native App 📱
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The assignment was to have an introduction to React Native. I choose to make a simple app that fetches from API and displays the data received.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I used Redux store to store the response from API. I used some React Native components to display the information and styled them with styled components.
 
 ## View it live
-
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- components/MoreInfo.js
+++ components/MoreInfo.js
@@ -1,7 +1,36 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { View, Text, Button } from "react-native";
 import * as WebBrowser from "expo-web-browser";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  display: flex;
+  flex-direction: column;
+  background-color: #f0f0f0;
+  justify-content: center;
+  margin: 50px;
+`;
+
+const Button = styled.TouchableOpacity`
+  display: flex;
+  justify-content: center;
+  width: 170px;
+  height: 40px;
+  background-color: #1a3aaa;
+  text-align: center;
+  border-radius: 10px;
+  padding: 5px 0;
+  margin: 50px auto;
+`;
+
+const InfoText = styled.Text`
+  margin-top: 10px;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+  margin: 0 auto;
+`;
 
 const MoreInfo = () => {
   const store = useSelector((store) => store.paintings.singleArt);
@@ -11,17 +40,16 @@ const MoreInfo = () => {
   };
 
   return (
-    <View>
-      <Text>Title: {store.title}</Text>
-      <Text>Author: {store.artistDisplayName}</Text>
-      <Text>Year: {store.objectEndDate}</Text>
-      <Text>Authors bio: {store.artistDisplayBio}</Text>
-      <Text>Medium: {store.medium}</Text>
-      <Button
-        title="More info from MET"
-        onPress={() => handleOpenWithWebBrowser()}
-      />
-    </View>
+    <Container>
+      <InfoText>Title: {store.title}</InfoText>
+      <InfoText>Author: {store.artistDisplayName}</InfoText>
+      <InfoText>Year: {store.objectEndDate}</InfoText>
+      <InfoText>Authors bio: {store.artistDisplayBio}</InfoText>
+      <InfoText>Medium: {store.medium}</InfoText>
+      <Button title="" onPress={() => handleOpenWithWebBrowser()}>
+        <ButtonText>More info from MET</ButtonText>
+      </Button>
+    </Container>
   );
 };
 
