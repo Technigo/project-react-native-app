6cd3643edbe6f64f8d7c8f1120a7eb1c9aada305
vrill <52007698+vrill@users.noreply.github.com>
2021-12-31 14:02:41 +0100

    Forgot to save the file for the previous commit. Oops. :)

--- components/ShakeApi.js
+++ components/ShakeApi.js
@@ -1,8 +1,44 @@
 import React from "react";
 
+import { Accelerometer } from "expo-sensors";
+
 const ShakeApi = () => {
+
+    const [data, setData] = useState({
+        x: 0,
+        y: 0,
+        z: 0,
+    });
+    const [subscription, setSubscription] = useState(null);
+
+    const _slow = () => {
+        Accelerometer.setUpdateInterval(1000);
+    };
+
+    const _fast = () => {
+        Accelerometer.setUpdateInterval(16);
+    };
+
+    const _subscribe = () => {
+        setSubscription(
+        Accelerometer.addListener(accelerometerData => {
+            setData(accelerometerData);
+        })
+        );
+    };
+
+    const _unsubscribe = () => {
+        subscription && subscription.remove();
+        setSubscription(null);
+    };
+
+    useEffect(() => {
+        _subscribe();
+        return () => _unsubscribe();
+    }, []);
+
     return (
-        
+
     )
 }
 
