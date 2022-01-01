2600e1ae0963f75c5274bf5e92351ff7538b3c47
silvertejp89 <kriss.stockhaus@gmail.com>
2022-01-01 13:56:42 +0100

    drawer navigation

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
 
 import ButtonApi from './components/ButtonApi';
@@ -18,12 +20,23 @@ const Title = styled.Text`
 `;
 
 const App = () => {
+	const Drawer = createDrawerNavigator();
+
+	// return (
+	// 	<Container>
+	// 		<Title>This is the best Kriss' cool app!</Title>
+	// 		<ShakeApi/>
+	// 	</Container>
+	// );
+
 	return (
-		<Container>
-			<Title>This is the best Kriss' cool app!</Title>
-			<ShakeApi/>
-		</Container>
-	);
+		<NavigationContainer>
+		  <Drawer.Navigator initialRouteName="Button">
+			<Drawer.Screen name="Button" component={ButtonApi} />
+			<Drawer.Screen name="Shake" component={ShakeApi} />
+		  </Drawer.Navigator>
+		</NavigationContainer>
+	  );
 };
 
 export default App;
