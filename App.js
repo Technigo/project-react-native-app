fdbe16883b3c1a9cac6f24f3d1071fa2f2be40df
Emma Lindell <emma.lindell4@gmail.com>
2022-04-25 22:43:08 +0200

    btn styling

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from "react"
-import { View, Text, Button, Image } from "react-native"
+import { View, Text, Button, Image, Wrapper } from "react-native"
 import styled from 'styled-components/native'
 
 import LottieView from 'lottie-react-native'
-
-import Header from "./components/Header"
-import Counter from "./components/Counter"
 
 import { DOG_URL } from "./utils/urls"
 
@@ -30,26 +27,33 @@ console.log(dog)
 	return (
 		<StyledView>
 
-		<Header title="Dog Finder" />
+
 		{/* <LottieView 
 		source={require('./assets/animation.json')} 
 		autoPlay/> */}
 
-		<Counter />
+
+		{/* {dog[0].bred_for 
+			: 
+			<Text>{dog[0].bred_for</Text>
+			<Image source={dog.url} /> 
+			? 
+			<Text>{dog.weight}</Text>} */}
+
+			<WrapThis>
+			<Title>Dog Finder</Title>
+				<DogImage
+				source={dog.url} />
+
+				{/* <Text>{dog.weight}</Text>
+
+				<Text>{dog.bred_for}</Text> */}
 
 
-		{/* {dog.url : <Image source={dog.url} /> ? <Text>{dog.weight}</Text>} */}
-
-		<DogImage
-        source={dog.url} />
-
-		<Text>{dog.weight}</Text>
-
-        <Text>{dog.bred_for}</Text>
-
-
-		<Button title="New dog" onPress={generateDog} />
-
+				<StyledButton onPress={generateDog}>
+				<TextButton>New doggie</TextButton>
+				</StyledButton>
+			</WrapThis>
 		</StyledView>
 	)
 }
@@ -62,12 +66,43 @@ flex: 1;
 background-color: papayawhip;
 align-items: "center";
 justify-content: "space-around";
+padding: 10px;
+`
+
+const Title = styled.Text`
+color: #ad8850;
+font-size: 28px;
+margin-bottom: 1em;
+font-weight: bold;
+`
+
+const WrapThis = styled.View`
+background: #e7cba499;
+padding: 2em;
+border-radius: 40;
+text-align: center;
+width: 100%;
 `
 
 const DogImage = styled.Image`
-width: 200;
-height: 200;
+width: 290;
+height: 290;
 justify-content: center;
 text-align: center;
+border-radius: 10px;
 `
+
+const StyledButton = styled.TouchableOpacity`
+  background-color: white;
+  border-radius: 10px;
+  padding:20px;
+  margin-top: 40px
+`
+
+const TextButton = styled.Text`
+  font-size: 18px;
+  color: #C68507;
+  `
+
+
 
