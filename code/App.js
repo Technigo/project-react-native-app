da43cf3b49eb400d373111410f4f74be8f87e094
Arianna Allahyar <anallahyar@gmail.com>
2022-04-25 14:45:34 +0200

    Basic display of recipes and scroll view

--- code/App.js
+++ code/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React, { useEffect, useState } from "react";
+import { Image, Button, ScrollView } from "react-native";
+import styled from "styled-components/native";
+import * as WebBrowser from "expo-web-browser";
 
 const Container = styled.View`
-	flex: 1;
-	background-color: papayawhip;
-	justify-content: center;
-	align-items: center;
+  width: 300px;
+  margin: 0 auto;
+  padding: 50px 0;
 `;
 
 const Title = styled.Text`
-	font-size: 24px;
-	color: palevioletred;
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const Recipe = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const Date = styled.Text`
+  font-style: italic;
+  color: grey;
 `;
 
 const App = () => {
-	return (
-		<Container>
-			<Title>This is your cool app!</Title>
-			<Title>Go to App.js and start coding</Title>
-			<Title>💅💅💅</Title>
-		</Container>
-	);
+  const [recipes, setRecipes] = useState([]);
+  const URL =
+    "https://content.guardianapis.com/food/series/yotam-ottolenghi-recipes?ids=food&&show-fields=thumbnail&api-key=2a13c9c5-db5c-48f4-b672-22e4e94ea6b5";
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => setRecipes(
+				data.response.leadContent.map((recipe) => ({
+					id: recipe.id,
+					title: recipe.webTitle,
+					date: recipe.webPublicationDate,
+					thumbnail: recipe.fields.thumbnail,
+					url: recipe.webUrl,
+				})),
+			))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <ScrollView style={{ marginTop: 16 }}>
+      <Container>
+        <Title>Yotam Ottolenghi Recipes from The Guardian</Title>
+        {recipes.map((recipe) => {
+          return (
+            <>
+              <Image
+                source={{ uri: recipe.thumbnail }}
+                style={{ width: 300, height: 300, marginTop: 24 }}
+              />
+              <Recipe key={recipe.id}>{recipe.title}</Recipe>
+              <Date>{recipe.date}</Date>
+              <Button
+                title="Link to recipe"
+                onPress={() => {
+                  WebBrowser.openBrowserAsync(recipe.url);
+                }}
+              />
+            </>
+          );
+        })}
+      </Container>
+    </ScrollView>
+  );
 };
 
 export default App;
