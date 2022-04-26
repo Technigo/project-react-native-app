cd2917ddce1fb9b109f9b49d55bed1d11e4631be
Suki Nhung Phan <nhungphan@Joel-Lov.local>
2022-04-27 00:32:24 +0300

    fetch api succesfully

--- App.js
+++ App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components/native';
+import { Text } from 'react-native';
+import { ScrollView, Dimensions, View, Image } from 'react-native';
+import Book from './src/Book';
+//import * as Speech from 'expo-speech';
+//import * as Sharing from 'expo-sharing';
 
 const Container = styled.View`
 	flex: 1;
 	background-color: papayawhip;
 	justify-content: center;
 	align-items: center;
+	padding: 40px;
 `;
 
-const Title = styled.Text`
-	font-size: 24px;
-	color: palevioletred;
-`;
+const Slider = styled.ScrollView`
+	width: ${props => props.width};
+	height: ${props => props.height};
+`
+
+const {width} = Dimensions.get('window');
+const height = width * 0.6;
 
 const App = () => {
+	const [popularBooks, setPopularBooks] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=L5ECJx1A2ZJ7sZfl75Q1v5QmD1FooUTj')
+		.then(res => res.json())
+		.then(data => setPopularBooks(data.results.books))
+	}, [])
+
 	return (
 		<Container>
-			<Title>This is your cool app!</Title>
-			<Title>Go to App.js and start coding</Title>
-			<Title>💅💅💅</Title>
+			<Slider 
+				horizontal 
+				pagingEnabled 
+				showsHorizontalScrollIndicator={false} 
+				width={width}
+				height={height}
+			>
+			{popularBooks.map(item =><Book book={item}/>)}
+			</Slider>	
 		</Container>
 	);
 };
 
 export default App;
+
+//Map over array, display the book
+
