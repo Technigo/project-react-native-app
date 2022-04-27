b1c52b33726acebbf2566bcb05dd15a966eb1061
frida axelsson <axelsson.frida@gmail.com>
2022-04-27 15:56:19 +0200

    added styling

--- components/SensorComponent.js
+++ components/SensorComponent.js
@@ -2,13 +2,52 @@ import React, { useState, useEffect } from "react";
 import { Accelerometer } from "expo-sensors";
 import styled from "styled-components/native";
 
-import { View, Text, TouchableOpacity } from "react-native";
-
 import { URL } from "./utils/urls";
+
+const Container = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+  margin: 40px;
+  padding: 0;
+`;
+const Quote = styled.Text`
+  font-size: 35;
+  line-height: 45;
+  text-align: left;
+`;
+const AuthorContainer = styled.View`
+  flex-direction: row;
+  text-align: left;
+  width: 300px;
+  margin: 15px;
+`;
+const Author = styled.Text`
+  font-style: italic;
+  font-size: 15;
+`;
+const Button = styled.TouchableOpacity`
+  background-color: #ffaf7b;
+  padding: 20px;
+  border-radius: 10px;
+  width: 170px;
+  align-items: center;
+  position: absolute;
+  bottom: 28px;
+  margin-bottom: 10px;
+`;
+const ButtonText = styled.Text`
+font-size: 15;
+font-weight: 500;
+`
+const SmallText = styled.Text`
+position: absolute;
+  bottom: 10px;
+`
 
 const isShaking = (data) => {
   const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z);
-  return totalForce > 1.80;
+  return totalForce > 1.8;
 };
 
 export const SensorComponent = () => {
@@ -53,13 +92,17 @@ export const SensorComponent = () => {
   }, []);
 
   return (
-    <View>
-      <View>{isShaking(data) && DisplayQuote()}</View>
-      <Text>{quote.content}</Text>
-      <Text>{quote.author}</Text>
-      <TouchableOpacity onPress={DisplayQuote}>
-        <Text>Quote</Text>
-      </TouchableOpacity>
-    </View>
+    <Container>
+      {isShaking(data) && DisplayQuote()}
+      <Quote>{quote.content}</Quote>
+      <AuthorContainer>
+        <Author>-{quote.author}</Author>
+      </AuthorContainer>
+
+      <Button onPress={DisplayQuote}>
+        <ButtonText>New Quote</ButtonText>
+      </Button>
+        <SmallText>Or Shake your phone to get a new Quote</SmallText>
+    </Container>
   );
 };
