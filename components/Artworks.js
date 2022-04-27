0df85c2c5d87813c08411459cd76dff14265f4b3
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-04-27 19:56:00 +0200

    added shake sensor

--- components/Artworks.js
+++ components/Artworks.js
@@ -1,22 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import styled from 'styled-components/native';
+import { Accelerometer } from "expo-sensors";
 
 import { REMBRANDT_URL } from '../utils/urls';
 
-const ArtworkListContainer = styled.ScrollView`
+const ArtworkContainer = styled.ScrollView`
 	background-color: papayawhip;
   margin: 50px;
 `
 
 const ArtworkImage = styled.Image`
-  width: 100px;
-  height: 100px;
+  width: 300px;
+  height: 300px;
 `
 
 const Artworks = () => {
 
   const [artworks, setArtworks] = useState([])
+  const [subscription, setSubscription] = useState(null);
+  //const [loading, setLoading] = useState(false)
+
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
 
   const generateArtworks = () => {
     fetch(REMBRANDT_URL)
@@ -28,21 +37,51 @@ const Artworks = () => {
     generateArtworks();
   }, []);
 
+  const subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener(accelerometerData => {
+        setData(accelerometerData);
+      })
+    );
+  };
+
+  const unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+  subscribe();
+  return () => unsubscribe();
+  }, []);
+
+  const isShaking = (data) => {
+    const totalForce = Math.abs(data.x) + Math.abs(data.y) + Math.abs(data.z);
+    return totalForce > 1.78;
+  }
+
+  useEffect(()=> {
+    if (isShaking(data)) {
+        generateArtworks();
+    }
+  }, [data])
+
   console.log(artworks)
-  // const randomArtwork = artworks[Math.floor(Math.random()*artworks.length)];
-  // console.log(randomArtwork)
+
 
   return (
-    <ArtworkListContainer>
+    <ArtworkContainer>
       <View>
         <Text>{artworks.longTitle}</Text>
         <ArtworkImage source={artworks.webImage}/>
+        <Button title="read more" />
+        <Button title="share" />
       </View>
-    </ArtworkListContainer>
+    </ArtworkContainer>
   )
 
   // return (
-  //   <ArtworkListContainer>
+  //   <ArtworkContainer>
   //   {artworks.map((artwork) => {
   //     return (
   //       <View key={artwork.id}>
@@ -51,7 +90,7 @@ const Artworks = () => {
   //       </View>
   //     )
   //   })}
-  //   </ArtworkListContainer>
+  //   </ArtworkContainer>
   // )
 };
 
