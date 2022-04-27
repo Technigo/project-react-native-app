4a1e70a7ac4210b9af86a01ef42c9707487c64c1
Jenfi72 <jennyfiskaare@Jennys-MacBook-Air.local>
2022-04-27 23:35:52 +0200

    added Header

--- App.js
+++ App.js
@@ -1,23 +1,45 @@
 import React from "react";
 import styled from "styled-components/native";
+
+
 import { SensorComponent } from "./components/SensorComponent";
+import { Header } from "./components/Header";
+
+
+
 
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
+  background-color: tomato;
   justify-content: center;
   align-items: center;
 `;
 
 const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
+  font-size: 32px;
+  color: white;
+  font-weight:bold;
+  text-align:center;
 `;
+
+const Title2= styled.Text`
+  font-size: 20px;
+  color: white;
+  text-align:center;
+`;
+
 
 const App = () => {
   return (
+
     <Container>
-      <SensorComponent></SensorComponent>
+
+<Title>how many steps? </Title>
+<Title2>track yourself</Title2>
+<Header></Header>
+      <SensorComponent>
+    </SensorComponent>
+
     </Container>
   );
 };
---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text } from "react-native";
+
+
+
+
+export const Header = () => {
+
+    return (
+
+<Text>hej</Text>
+
+    );
+
+
+
+};
+
