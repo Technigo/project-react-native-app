69708e93929f9dcb2d6c007b1cfb608ba7b2aa19
EmmaaBerg <emmaberg0819@gmail.com>
2022-04-29 16:50:58 +0200

    added Lottie animation

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
-import Header from './components/Header';
+import Start from './components/Start';
 import ActivityApi from './components/ActivityApi';
 
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#FAF6F6',
+		backgroundColor: '#C5DECD',
 		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center'
-	}, 
+	},
 })
 
+
 const App = () => {
+
 	return (
 		<View style={styles.container}>
-			<Header/>
-			<ActivityApi/>
+			<Start />
+			<ActivityApi />
 		</View>
 	);
 };
---
+++ components/Start.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {StyleSheet, View, Text} from 'react-native'
+import LottieView from 'lottie-react-native'
+
+import animation from '../assets/animation.json'
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        marginBottom: 70
+	}, 
+	title: {
+		fontSize: 30,
+		color: 'black',
+        marginTop: 10,
+        padding: 70, 
+        
+	}, 
+    highlight: {
+        color: '#ED254E',
+        fontWeight: 'bold'
+    },
+    subtitle: {
+        fontSize: 18,
+        fontStyle: 'italic',
+        color: 'black',
+        marginTop: 70,
+    }, 
+})
+
+const Start = () => {
+
+    return(
+        <View style={styles.container}>
+           
+            <LottieView source={animation} autoPlay/>
+                <Text style={styles.title}>Are you <Text style={styles.highlight}>bored</Text> ?</Text>
+                <Text style={styles.subtitle}>Click the button and let's do something</Text>
+
+        </View>
+    )
+}
+
+export default Start
