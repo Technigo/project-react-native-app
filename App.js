60c6aa107d045dbf8f244d10f0f1e0765d9c980c
toal13 <tomoyo.tomoyo.m@gmail.com>
2022-04-29 11:54:33 +0200

    first push

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import styled from "styled-components/native";
-import { SensorComponent } from "./components/SensorComponent";
+import ShakeApi from "./components/ShakeApi";
+
 
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
+  background-color: #1B95D4;
   justify-content: center;
   align-items: center;
 `;
 
 const Title = styled.Text`
   font-size: 24px;
-  color: palevioletred;
+  color: orange;
+  font-weight: 700;
 `;
 
 const App = () => {
   return (
     <Container>
-      <SensorComponent></SensorComponent>
+      <ShakeApi />
+      <Title>Shake shake!</Title>
     </Container>
   );
 };
