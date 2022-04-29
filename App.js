4d5037bba006cfa75dfb5708037bfd94ed0b220c
Joanna Lodell <joannalodell@Joannas-MacBook-Air.local>
2022-04-29 14:54:49 +0200

    fetching API and styling

--- App.js
+++ App.js
@@ -1,25 +1,47 @@
 import React from "react";
 import styled from "styled-components/native";
 import { SensorComponent } from "./components/SensorComponent";
+import { ScrollView } from 'react-native'
 
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
+  background-color: powderblue;
   justify-content: center;
   align-items: center;
 `;
 
+const Header = styled.View`
+  height: 100px;
+  justify-content: center;
+  padding: 5px;
+  width: 100%;
+  align-items: center;
+  margin-bottom: 50px;
+  margin-top: 100px;
+  position: relative;
+`
+
 const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`;
+  font-size: 30px;
+  font-weight: 700;
+`
 
 const App = () => {
   return (
-    <Container>
-      <SensorComponent></SensorComponent>
-    </Container>
+    
+  <Container>
+    <ScrollView>
+      <Container>
+      <Header>
+        <Title>SHAKE FOR A CAT FACT</Title>
+      </Header>
+      <SensorComponent />
+      </Container>
+    </ScrollView>
+  </Container>
+    
   );
 };
 
 export default App;
+
